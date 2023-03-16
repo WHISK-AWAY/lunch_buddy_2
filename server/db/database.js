6577bfb7bc8bdf1a57d1a944571aa9a0ad29a3ca
inathan44 <inathan44@yahoo.com>
2023-03-16 11:48:13 -0400

    init database

---
+++ server/db/database.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const database_url =
+  process.env.DATABASE_URL || `postgres://localhost:5432/lunch_buddy`;
+
+const db = new Sequelize(
+  // loads the correct database url based on NODE_ENV (default / dev / test)
+  database_url,
+  {
+    logging: false,
+  }
+);
+
+module.exports = db;
