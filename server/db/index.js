b4ed8247b1a44bf09112970fbd68af745f482e1b
Patrick Bryan <pbryan9@gmail.com>
2023-03-16 11:51:32 -0700

    update models index for newly added models

---
+++ server/db/index.js
@@ -0,0 +1,23 @@
+const Category = require('./models/Category');
+const Meeting = require('./models/Meeting');
+const Message = require('./models/Message');
+const Rating = require('./models/Rating');
+const Tag = require('./models/Tag');
+const User = require('./models/User');
+
+/**
+ * ASSOCIATIONS
+ */
+
+
+
+
+
+module.exports = {
+  Category,
+  Meeting,
+  Message,
+  Rating,
+  Tag,
+  User
+}
