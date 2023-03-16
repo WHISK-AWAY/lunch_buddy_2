be17cdc9a90bf37ee9ba8061f908597530328d9a
inathan44 <inathan44@yahoo.com>
2023-03-16 10:50:00 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='flex flex-col w-screen h-screen justify-center items-center text-white text-5xl gap-4'>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>Increase</button>
+    </div>
+  );
+}
+
+export default App;
