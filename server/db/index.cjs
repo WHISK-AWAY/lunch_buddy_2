ecf865a0f168268f3ccf058fc4657b3a1cf820c2
Patrick Bryan <pbryan9@gmail.com>
2023-03-16 13:10:11 -0700

    initial associations

--- server/db/index.cjs
+++ server/db/index.cjs
@@ -1,3 +1,4 @@
+const db = require('./database.cjs');
 const Category = require('./models/Category.cjs');
 const Meeting = require('./models/Meeting.cjs');
 const Message = require('./models/Message.cjs');
@@ -8,6 +9,34 @@ const User = require('./models/User.cjs');
 /**
  * ASSOCIATIONS
  */
+
+User.belongsToMany(Tag, { through: 'user_tags' });
+Tag.belongsToMany(User, { through: 'user_tags' });
+
+Tag.belongsTo(Category);
+Category.hasMany(Tag);
+
+User.hasMany(Meeting);
+Meeting.belongsTo(User);
+
+/**
+ * TODO: I commented out the senderId in messages model --
+ * make sure the association/seeding works, and then go delete comments
+ */
+User.hasMany(Message, { foreignKey: 'senderId' });
+Message.belongsTo(User, { foreignKey: 'senderId' });
+
+Meeting.hasMany(Message);
+Message.belongsTo(Meeting);
+
+Meeting.hasMany(Rating);
+Rating.belongsTo(Meeting);
+
+Rating.belongsTo(User, { foreignKey: 'buddyId' });
+User.hasMany(Rating, { foreignKey: 'buddyId' });
+
+// pretty much just for testing
+db.sync({ force: true }).then(() => db.close());
 
 module.exports = {
   Category,
