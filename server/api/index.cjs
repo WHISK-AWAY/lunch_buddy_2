bd33d7c93fb0a705baf93c7279785633988afbfd
inathan44 <inathan44@yahoo.com>
2023-03-17 16:38:40 -0400

    Import tag router to api index

--- server/api/index.cjs
+++ server/api/index.cjs
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+router.use('/tags', require('./routes/tags.cjs'));
+
 // If api route isn't found
 router.use((req, res, next) => {
   const err = new Error('API ROUTE NOT FOUND!');
