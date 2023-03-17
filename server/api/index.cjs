1575c57f19490e270f8bae643f62be1228bd15e0
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-17 17:30:10 -0500

    put messages in index.cjs

--- server/api/index.cjs
+++ server/api/index.cjs
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 // If api route isn't found
+
+router.use('/meeting', require('./meeting/index.cjs'));
+
 router.use((req, res, next) => {
   const err = new Error('API ROUTE NOT FOUND!');
   err.status = 404;
