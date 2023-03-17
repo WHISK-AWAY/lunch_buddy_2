dbd28644ae6447dd45234a79d4b79fbf7640ccf1
inathan44 <inathan44@yahoo.com>
2023-03-17 17:21:35 -0400

    delete route

--- server/api/routes/tags.cjs
+++ server/api/routes/tags.cjs
@@ -24,13 +24,30 @@ router.put('/:tagId', isAdmin, async (req, res, next) => {
     if (!tag) {
       return res.status(404).send('No tag found to update');
     } else {
-      const updatedTag = await Tag.update(req.body);
+      const updatedTag = await tag.update(req.body);
       res.json(updatedTag);
     }
   } catch (error) {
     console.error(
       `Server issue when trying to update tag: ${req.params.tagId}. Please try again later.`
     );
+    next(error);
+  }
+});
+
+router.delete('/:tagId', async (req, res, next) => {
+  try {
+    const tag = await Tag.findByPk(req.params.tagId);
+    if (!tag) {
+      return res.status(404).send(`No tag found to delete`);
+    }
+    await tag.destroy();
+    res.json(tag);
+  } catch (error) {
+    console.error(
+      `Server issue when trying to delete tag: ${req.params.tagId}. Please try again later`
+    );
+    next(error);
   }
 });
 
