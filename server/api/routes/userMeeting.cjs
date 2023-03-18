91f2349f2e0d25edee4fa5773cf012f021d45aa4
inathan44 <inathan44@yahoo.com>
2023-03-18 18:17:13 -0400

    getall, getone, change date/cancel routes

--- server/api/routes/userMeeting.cjs
+++ server/api/routes/userMeeting.cjs
@@ -1,33 +1,107 @@
 const router = require('express').Router({ mergeParams: true });
+const { Meeting } = require('../../db/index.cjs');
+const { requireToken, sameUserOrAdmin } = require('../authMiddleware.cjs');
 
-router.get('/', (req, res, next) => {
+router.get('/', requireToken, sameUserOrAdmin, async (req, res, next) => {
   /**
    * GET /api/user/:userId/meeting
    * return a list of all meetings associated with userId
    */
-
-  res.send(`hello from /api/user/${req.params.userId}/meeting`);
+  try {
+    const userMeetings = await Meeting.findAll({
+      where: {
+        userId: req.params.userId,
+      },
+    });
+    res.status(200).json(userMeetings);
+  } catch (error) {
+    console.error('Error retrieving meetings, please try again later.');
+    next(error);
+  }
 });
 
-router.put('/:meetingId', (req, res, next) => {
-  /**
-   * PUT /api/user/:userId/meeting/:meetingId
-   * update details of given meeting ID
-   */
-  res.send(
-    `hello from /api/user/${req.params.userId}/meeting/${req.params.meetingId}`
-  );
-});
+router.get(
+  '/:meetingId',
+  requireToken,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    /**
+     * GET /api/user/:userId/meeting/:meetingId
+     * get details of a single meeting
+     */
+    try {
+      const meeting = await Meeting.findByPk(req.params.meetingId);
+      if (!meeting) {
+        return res.status(404).send('No meeting found.');
+      } else if (req.user.id !== meeting.userId && req.user.role !== 'admin') {
+        res.status(403).send('You are unable to view this meeting.');
+      } else {
+        res.status(200).json(meeting);
+      }
+    } catch (error) {
+      console.error(
+        'Error trying to view meeting date. Please try again later.'
+      );
+      next(error);
+    }
+  }
+);
 
-router.put('/:meetingId/cancel', (req, res, next) => {
-  /**
-   * PUT /api/user/:userId/meeting/:meetingId/cancel
-   * set meeting status to 'closed'
-   */
+router.put(
+  '/:meetingId/date',
+  requireToken,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    /**
+     * PUT /api/user/:userId/meeting/:meetingId/date
+     * update date of given meeting ID
+     */
+    const { lunchDate } = req.body;
+    try {
+      const meeting = await Meeting.findByPk(req.params.meetingId);
+      if (!meeting) {
+        return res.status(404).send('No meeting found to update.');
+      } else if (req.user.id !== meeting.userId && req.user.role !== 'admin') {
+        res.status(403).send('You are unable to edit this meeting.');
+      } else {
+        const updatedMeeting = await meeting.update({ lunchDate });
+        res.status(200).json(updatedMeeting);
+      }
+    } catch (error) {
+      console.error(
+        'Error trying to change meeting date. Please try again later.'
+      );
+      next(error);
+    }
+  }
+);
 
-  res.send(
-    `hello from /api/user/${req.params.userId}/meeting/${req.params.meetingId}/cancel`
-  );
-});
+router.put(
+  '/:meetingId/cancel',
+  requireToken,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    /**
+     * PUT /api/user/:userId/meeting/:meetingId/cancel
+     * set meeting status to 'closed'
+     */
+    try {
+      const meeting = await Meeting.findByPk(req.params.meetingId);
+      if (!meeting) {
+        return res.status(404).send('No meeting found to update.');
+      } else if (req.user.id !== meeting.userId && req.user.role !== 'admin') {
+        res.status(403).send('You are unable to edit this meeting.');
+      } else {
+        const updatedMeeting = await meeting.update({ isClosed: true });
+        res.status(200).json(updatedMeeting);
+      }
+    } catch (error) {
+      console.error(
+        'Error trying to change meeting date. Please try again later.'
+      );
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
