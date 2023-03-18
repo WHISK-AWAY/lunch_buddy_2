e26f1b7b1d8967bd7385a2156df984dd33602631
Patrick Bryan <pbryan9@gmail.com>
2023-03-18 07:33:44 -0700

    add auth route to router index

--- server/api/index.cjs
+++ server/api/index.cjs
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+router.use('/auth', require('./routes/auth.cjs'));
 router.use('/user', require('./routes/user.cjs'));
 router.use('/tags', require('./routes/tags.cjs'));
 
