1143b6d80f72d4494afad107f784ded227c26331
Patrick Bryan <pbryan9@gmail.com>
2023-03-18 07:46:16 -0700

    add get /, post /, get:id user routes

--- server/api/routes/user.cjs
+++ server/api/routes/user.cjs
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-// const {requireToken, isAdmin} = require('./authMiddleware.cjs');
+const {
+  requireToken,
+  isAdmin,
+  sameUserOrAdmin,
+} = require('../authMiddleware.cjs');
 
-const { User } = require('../../db/index.cjs');
+const { User, Tag } = require('../../db/index.cjs');
 
-router.get('/', async (req, res, next) => {
+router.get('/', requireToken, isAdmin, async (req, res, next) => {
   /**
    * GET /api/user
    * Admin-only: return a listing of all users
@@ -28,16 +32,72 @@ router.post('/', (req, res, next) => {
    * POST /api/user
    * Create new user
    */
-  res.send('hello');
+  try {
+    // destructure to filter out any other weird things that might be
+    // passed along on the body object
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      age,
+      gender,
+      address1,
+      address2,
+      city,
+      state,
+      zip,
+      avatarUrl,
+      aboutMe,
+      tags,
+    } = req.body;
+
+    const newUserData = {
+      firstName,
+      lastName,
+      email,
+      password,
+      age,
+      gender,
+      address1,
+      address2,
+      city,
+      state,
+      zip,
+      avatarUrl,
+      aboutMe,
+    };
+
+    res.send('hello');
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/:userId', (req, res, next) => {
-  /**
-   * GET /api/user/:userId
-   * return details of given user
-   */
-  res.send('hello');
-});
+router.get(
+  '/:userId',
+  requireToken,
+  sameUserOrAdmin,
+  async (req, res, next) => {
+    /**
+     * GET /api/user/:userId
+     * return details of given user
+     */
+    try {
+      const userId = +req.params.userId;
+      const user = await User.findByPk(userId, {
+        include: [Tag],
+        attributes: {
+          exclude: ['password', 'avgRating', 'reportCount', 'strikeCount'],
+        },
+      });
+      if (!user) return res.status(404).send(`No such user id: ${userId}`);
+      res.status(200).json(user);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 router.put('/:userId', (req, res, next) => {
   /**
@@ -47,12 +107,11 @@ router.put('/:userId', (req, res, next) => {
   res.send('hello');
 });
 
-router.delete('/:userId', (req, res, next) => {
+router.delete('/:userId', requireToken, isAdmin, (req, res, next) => {
   /**
    * DELETE /api/user/:userId
    * Admin-only: delete user account
    */
-  res.send('hello');
 });
 
 router.put('/:userId/location', (req, res, next) => {
