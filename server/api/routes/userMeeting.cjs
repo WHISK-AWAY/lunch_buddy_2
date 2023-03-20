af621bffd23ff74b0bdc4fec56ddebb0838eb0bf
inathan44 <inathan44@yahoo.com>
2023-03-20 01:11:13 -0400

    routes return buddy meetings, check valid user

--- server/api/routes/userMeeting.cjs
+++ server/api/routes/userMeeting.cjs
@@ -1,45 +1,63 @@
 const { Op } = require('sequelize');
-
 const router = require('express').Router({ mergeParams: true });
 const { Meeting } = require('../../db/index.cjs');
 const { requireToken, sameUserOrAdmin } = require('../authMiddleware.cjs');
+const validUser = require('../validUserMiddleware.cjs');
 
-router.get('/', requireToken, sameUserOrAdmin, async (req, res, next) => {
-  /**
-   * GET /api/user/:userId/meeting
-   * return a list of all meetings associated with userId
-   */
-  try {
-    const userMeetings = await Meeting.findAll({
-      where: {
-        [Op.or]: [
-          { userId: req.params.userId },
-          { buddyId: req.params.userId },
-        ],
-      },
-    });
-    res.status(200).json(userMeetings);
-  } catch (error) {
-    console.error('Error retrieving meetings, please try again later.');
-    next(error);
+router.get(
+  '/',
+  requireToken,
+  sameUserOrAdmin,
+  validUser,
+  async (req, res, next) => {
+    /**
+     * GET /api/user/:userId/meeting
+     * return a list of all meetings associated with userId
+     */
+    try {
+      const userMeetings = await Meeting.findAll({
+        where: {
+          [Op.or]: [
+            { userId: req.params.userId },
+            { buddyId: req.params.userId },
+          ],
+        },
+      });
+      res.status(200).json(userMeetings);
+    } catch (error) {
+      console.error('Error retrieving meetings, please try again later.');
+      next(error);
+    }
   }
-});
+);
 
 router.get(
   '/:meetingId',
   requireToken,
   sameUserOrAdmin,
+  validUser,
   async (req, res, next) => {
     /**
      * GET /api/user/:userId/meeting/:meetingId
      * get details of a single meeting
      */
     try {
-      const meeting = await Meeting.findByPk(req.params.meetingId);
+      const meeting = await Meeting.findOne({
+        where: {
+          [Op.or]: [
+            { userId: req.params.userId },
+            { buddyId: req.params.userId },
+          ],
+          id: req.params.meetingId,
+        },
+      });
       if (!meeting) {
-        return res.status(404).send('No meeting found.');
+        return res
+          .status(404)
+          .send('No meeting found matching this user and meetingId.');
       } else if (
-        (req.user.id !== meeting.userId || req.user.id !== meeting.buddyId) &&
+        req.user.id !== meeting.userId &&
+        req.user.id !== meeting.buddyId &&
         req.user.role !== 'admin'
       ) {
         res.status(403).send('You are unable to view this meeting.');
@@ -59,6 +77,7 @@ router.put(
   '/:meetingId/date',
   requireToken,
   sameUserOrAdmin,
+  validUser,
   async (req, res, next) => {
     /**
      * PUT /api/user/:userId/meeting/:meetingId/date
@@ -66,11 +85,22 @@ router.put(
      */
     const { lunchDate } = req.body;
     try {
-      const meeting = await Meeting.findByPk(req.params.meetingId);
+      const meeting = await Meeting.findOne({
+        where: {
+          [Op.or]: [
+            { userId: req.params.userId },
+            { buddyId: req.params.userId },
+          ],
+          id: req.params.meetingId,
+        },
+      });
       if (!meeting) {
-        return res.status(404).send('No meeting found to update.');
+        return res
+          .status(404)
+          .send('No meeting found matching user and meetingId to update.');
       } else if (
-        (req.user.id !== meeting.userId || req.user.id !== meeting.buddyId) &&
+        req.user.id !== meeting.userId &&
+        req.user.id !== meeting.buddyId &&
         req.user.role !== 'admin'
       ) {
         res.status(403).send('You are unable to edit this meeting.');
@@ -91,16 +121,31 @@ router.put(
   '/:meetingId/cancel',
   requireToken,
   sameUserOrAdmin,
+  validUser,
   async (req, res, next) => {
     /**
      * PUT /api/user/:userId/meeting/:meetingId/cancel
      * set meeting status to 'closed'
      */
     try {
-      const meeting = await Meeting.findByPk(req.params.meetingId);
+      const meeting = await Meeting.findOne({
+        where: {
+          [Op.or]: [
+            { userId: req.params.userId },
+            { buddyId: req.params.userId },
+          ],
+          id: req.params.meetingId,
+        },
+      });
       if (!meeting) {
-        return res.status(404).send('No meeting found to update.');
-      } else if (req.user.id !== meeting.userId && req.user.role !== 'admin') {
+        return res
+          .status(404)
+          .send('No meeting found matching user and meetingId to cancel.');
+      } else if (
+        req.user.id !== meeting.userId &&
+        req.user.id !== meeting.buddyId &&
+        req.user.role !== 'admin'
+      ) {
         res.status(403).send('You are unable to edit this meeting.');
       } else {
         const updatedMeeting = await meeting.update({ isClosed: true });
