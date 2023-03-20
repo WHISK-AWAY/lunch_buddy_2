6b497c0f1ad0deb7b0a19195b8efabae52291779
inathan44 <inathan44@yahoo.com>
2023-03-20 19:51:56 -0400

    added routes, route to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex flex-col w-screen h-screen justify-center items-center text-white text-xl gap-4">
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount((prev) => prev + 2)}>Increase</button>
+    <div>
+      <Routes>
+        <Route path="/" element={<h1>Testing</h1>}></Route>
+      </Routes>
     </div>
   );
 }
