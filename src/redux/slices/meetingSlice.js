64700baf600fa828546a6c66c4cb71119b169d7d
inathan44 <inathan44@yahoo.com>
2023-03-21 17:21:23 -0400

    create meeting thunk

--- src/redux/slices/meetingSlice.js
+++ src/redux/slices/meetingSlice.js
@@ -1,11 +1,29 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+export const createMeeting = createAsyncThunk(
+  'meeting/createMeeting',
+  async ({ token, newMeeting }, { rejectWithValue }) => {
+    try {
+      const { data } = axios.post('/api/meeting', newMeeting, {
+        headers: {
+          authorization: token,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      return rejectWithValue(error);
+    }
+  }
+);
+
 const meetingSlice = createSlice({
   name: 'meetings',
   initialState: {
     meetings: [],
     meeting: {},
+    isLoading: false,
+    error: '',
   },
   reducers: {
     testMeeting: (state) => {
