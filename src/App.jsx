cad33fbfaaf865d4b92ac7925442fc50d534e1bd
Patrick Bryan <pbryan9@gmail.com>
2023-03-21 10:20:19 -0700

    remove slice testing from App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import MockResponsive from './components/MockResponsive';
 
-import { requestLogin, tryToken, selectAuth } from './redux/slices';
-
 function App() {
-  const dispatch = useDispatch();
-  const auth = useSelector(selectAuth);
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    dispatch(
-      requestLogin({
-        email: 'lshelton0@gmail.com',
-        password: 'BJB6qr7fdsfsd4ee4',
-      })
-    );
-  }, []);
-
-  console.log('auth:', auth);
-
   return (
     <div>
       <Routes>
