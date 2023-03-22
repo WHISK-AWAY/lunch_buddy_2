6a7412274c6ed5279f2db28129ab77fd0cfa3718
inathan44 <inathan44@yahoo.com>
2023-03-22 14:58:40 -0400

    about yourself route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MockResponsive from './components/MockResponsive';
+import AboutForm from './pages/aboutYourself/AboutForm';
 import RegisterForm from './pages/register/RegisterForm';
 import SignInForm from './pages/signIn/SignInForm';
 
@@ -11,8 +12,9 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<MockResponsive />} />
-      <Route path="login" element={<SignInForm />} />
-      <Route path="register" element={<RegisterForm />} />
+      <Route path="/login" element={<SignInForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/register/aboutyourself" element={<AboutForm />} />
     </Routes>
   );
 }
