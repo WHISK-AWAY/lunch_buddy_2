80388ea52fed879dec49c57a8b53087b8c67ba4f
inathan44 <inathan44@yahoo.com>
2023-03-22 14:14:16 -0400

    Updated styling to match mockup

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -24,19 +24,18 @@ const RegisterForm = () => {
     console.log(formInputs);
   };
   return (
-    <div className=" bg-orange-100 flex justify-center items-center md:h-screen">
-      <div className="w-full mx-4 my-12 max-w-4xl">
-        <form className="bg-white p-10 rounded-lg shadow-lg min-w-full md:grid md:grid-cols-2 md:gap-x-8">
-          <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans md:col-span-2">
-            Register
+    <div className="flex justify-center items-center md:h-screen">
+      <div className="w-full my-12 max-w-4xl">
+        <form className="bg-white grid grid-cols-6 justify-center mx-4 gap-x-2 gap-y-6">
+          <h1 className="text-center text-2xl mb-6 text-red-400 font-bold font-sans col-span-full">
+            Sign Up
           </h1>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-3 w-full">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               First Name
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="Ulysses"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.firstName}
               onChange={(e) =>
                 setFormInputs((prev) => ({
@@ -46,52 +45,48 @@ const RegisterForm = () => {
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-3 w-full">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Last Name
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="The Third"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.lastName}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, lastName: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-full">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Email
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="Email"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.email}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, email: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-full">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Password
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="********"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.password}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, password: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-full">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Confirm Password
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="********"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.confirmPassword}
               onChange={(e) =>
                 setFormInputs((prev) => ({
@@ -101,57 +96,54 @@ const RegisterForm = () => {
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-4">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Address 1
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="123 Main St"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.address1}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, address1: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-2">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Address 2
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="Apt 404"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.address2}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, address2: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-4">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               City
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="New York"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.city}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, city: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-2">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               State
             </label>
             <select
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700 text-sm"
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, state: e.target.value }))
               }
             >
               <option disabled selected value>
-                - Choose One -
+                {''}
               </option>
               <option>Test</option>
               <option>Test2</option>
@@ -161,45 +153,43 @@ const RegisterForm = () => {
               <option>Test6</option>
             </select>
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-2">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Zip
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
-              placeholder="12345"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.zip}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, zip: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-2">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Age
             </label>
             <input
               type="number"
-              placeholder="25"
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               value={formInputs.age}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, age: e.target.value }))
               }
             />
           </div>
-          <div>
-            <label className="text-gray-800 font-semibold block my-2 text-sm">
+          <div className="relative col-span-2">
+            <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
               Gender
             </label>
             <select
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-300"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none h-10 border border-slate-700"
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, gender: e.target.value }))
               }
             >
               <option disabled selected value>
-                - Choose One -
+                {''}
               </option>
               <option>Male</option>
               <option>Female</option>
@@ -208,7 +198,7 @@ const RegisterForm = () => {
               <option>Prefer Not To Say</option>
             </select>
           </div>
-          <div className="md:col-span-2 md:w-3/5 md:mx-auto">
+          <div className="col-span-full md:w-3/5 md:mx-auto">
             <FormButton handleSubmit={handleSubmit}>Create Account</FormButton>
           </div>
         </form>
