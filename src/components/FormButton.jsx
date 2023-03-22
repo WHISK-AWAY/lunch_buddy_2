e47b9ab1bdb4bf514157d75af7cf398a2dc0b4e2
inathan44 <inathan44@yahoo.com>
2023-03-22 02:14:18 -0400

    Form button component

---
+++ src/components/FormButton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FormButton = (props) => {
+  return (
+    <button
+      type="submit"
+      className="w-full mt-6 bg-red-500 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans hover:bg-red-600 active:bg-red-700 transition-all"
+      onClick={props.handleSubmit}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default FormButton;
