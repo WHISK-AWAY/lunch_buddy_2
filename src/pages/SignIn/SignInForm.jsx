3efe5db54bd0a017050284c7c8b4a68fcda268be
inathan44 <inathan44@yahoo.com>
2023-03-22 13:50:23 -0400

    Updated signin form to match mockup

--- src/pages/SignIn/SignInForm.jsx
+++ src/pages/SignIn/SignInForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import FormButton from '../../components/FormButton';
 
 const inputs = {
@@ -14,42 +15,40 @@ const SignInForm = () => {
     console.log(formInputs);
   };
   return (
-    <div className="h-screen bg-indigo-100 flex justify-center items-center">
-      <div className="lg:w-2/5 md:w-1/2 w-4/5 sm:3/5">
-        <form className="bg-white p-10 rounded-lg shadow-lg min-w-full">
-          <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">
-            Login
+    <div className="h-screen flex justify-center items-center">
+      <div className="w-full xs:w-4/5 sm:w-3/5 md:w-1/2 lg:w-2/5">
+        <form className="bg-white p-10 rounded-lg min-w-full">
+          <h1 className="text-center text-2xl mb-6 text-red-400 font-bold font-sans">
+            Sign In
           </h1>
-          <div>
+          <div className="relative my-6">
             <label
-              className="text-gray-800 font-semibold block my-3 text-md"
+              className="text-red-400 font-semibold block text-md absolute -top-3 left-3 bg-white px-1"
               htmlFor="email"
             >
               Email
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none border border-gray-800"
               type="text"
               name="email"
-              placeholder="Email"
               value={formInputs.email}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, email: e.target.value }))
               }
             />
           </div>
-          <div>
+          <div className="relative mt-6">
             <label
-              className="text-gray-800 font-semibold block my-3 text-md"
+              className="text-red-400 font-semibold block text-md absolute -top-3 left-3 bg-white px-1"
               htmlFor="password"
             >
               Password
             </label>
             <input
-              className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+              className="w-full px-4 py-2 rounded-lg focus:outline-none border border-gray-800"
               type="password"
               name="password"
-              placeholder="********"
               value={formInputs.password}
               onChange={(e) =>
                 setFormInputs((prev) => ({ ...prev, password: e.target.value }))
@@ -57,6 +56,12 @@ const SignInForm = () => {
             />
           </div>
           <FormButton handleSubmit={handleSubmit}>Sign In</FormButton>
+          <p className="my-4 text-xs sm:text-base text-center">
+            Don't have an account? Create one{' '}
+            <Link to={'/register'}>
+              <span className="text-red-400 hover:underline">here</span>
+            </Link>
+          </p>
         </form>
       </div>
     </div>
