1c7e0b984ed8d1c34b5305c6b319e34784223901
inathan44 <inathan44@yahoo.com>
2023-03-22 02:13:13 -0400

    added login/register routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MockResponsive from './components/MockResponsive';
+import RegisterForm from './pages/Register/RegisterForm';
+import SignInForm from './pages/SignIn/SignInForm';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<MockResponsive />}></Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<MockResponsive />} />
+      <Route path="login" element={<SignInForm />} />
+      <Route path="register" element={<RegisterForm />} />
+    </Routes>
   );
 }
 
