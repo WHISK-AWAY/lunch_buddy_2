8f6c338d921b194cfe74a19d3fb95e8f5d3bd623
Patrick Bryan <pbryan9@gmail.com>
2023-03-22 21:19:35 -0700

    remove new comps from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MockResponsive from './components/MockResponsive';
-import RegisterForm from './pages/Register/RegisterForm';
-import SignInForm from './pages/SignIn/SignInForm';
 
 function App() {
   return (
