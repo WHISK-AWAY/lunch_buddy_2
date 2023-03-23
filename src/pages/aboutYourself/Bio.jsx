3665ad53c81ff478740b16bc9403d5f5b71b1ee2
inathan44 <inathan44@yahoo.com>
2023-03-23 01:03:54 -0400

    controlled input

--- src/pages/aboutYourself/Bio.jsx
+++ src/pages/aboutYourself/Bio.jsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from 'react';
 
-const Bio = () => {
+const Bio = ({ setBio , bio}) => {
   return (
-    <div className="relative h-44 w-4/5">
+    <div className="relative mb-4 w-4/5">
       <label className="text-red-400 font-semibold block text-sm sm:text-base absolute -top-3 left-3 bg-white px-1">
         Bio
       </label>
       <textarea
         className="w-full px-4 rounded-2xl focus:outline-none border border-slate-700 h-24 py-4"
-        onChange={(e) =>
-          setFormInputs((prev) => ({ ...prev, password: e.target.value }))
-        }
+        onChange={(e) => setBio(e.target.value)}
+        value={bio}
       />
     </div>
   );
