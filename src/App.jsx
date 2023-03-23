d9ee45a91c1b52deb1efd92e09b39cd844796335
Patrick Bryan <pbryan9@gmail.com>
2023-03-22 19:01:25 -0700

    ok already

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MockResponsive from './components/MockResponsive';
+import RegisterForm from './pages/Register/RegisterForm';
+import SignInForm from './pages/SignIn/SignInForm';
 
 function App() {
   return (
