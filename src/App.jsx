748cfadc40e3891e8f4045c0ffddc0e397f27fa0
Patrick Bryan <pbryan9@gmail.com>
2023-03-23 10:26:23 -0700

    add routing for search/match group

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import MockResponsive from './components/MockResponsive';
+import {
+  MeetingSetup,
+  BuddyList,
+  MeetingRecap,
+  RestaurantSuggestions,
+} from './pages';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<MockResponsive />}></Route>
+        <Route path="/test" element={<MeetingSetup />}></Route>
+        <Route path="/match" element={<RestaurantSuggestions />}></Route>
+        <Route path="/match/results" element={<BuddyList />}></Route>
+        <Route
+          path="/match/restaurants"
+          element={<RestaurantSuggestions />}
+        ></Route>
+        <Route path="/match/confirm" element={<MeetingRecap />}></Route>
       </Routes>
     </div>
   );
