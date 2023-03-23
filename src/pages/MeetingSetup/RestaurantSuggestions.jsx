b5aaf12b5575813c19fc224b8f30e3a9e124b448
Patrick Bryan <pbryan9@gmail.com>
2023-03-23 10:25:41 -0700

    scaffolding for search/match group

---
+++ src/pages/MeetingSetup/RestaurantSuggestions.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function RestaurantSuggestions(props) {
+  return <Link to="/match/confirm">To Meeting Recap Page</Link>;
+}
