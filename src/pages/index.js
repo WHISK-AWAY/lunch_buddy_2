b9c170685c8f076bcac8bdfbf23f78edb85252a9
inathan44 <inathan44@yahoo.com>
2023-03-23 20:17:47 -0400

    imported navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as MeetingSetup } from './MeetingSetup/MeetingSetup';
 export { default as BuddyList } from './MeetingSetup/BuddyList';
 export { default as MeetingRecap } from './MeetingSetup/MeetingRecap';
 export { default as RestaurantSuggestions } from './MeetingSetup/RestaurantSuggestions';
+export { default as NavBar } from '../components/NavBar';
