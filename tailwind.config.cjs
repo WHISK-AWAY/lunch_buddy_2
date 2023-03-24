292cca80f4a568960cbc7b296f76160498745d06
ruralFluffyDill <stacylukavsky@gmail.com>
2023-03-23 20:10:08 -0400

    Fonts& compressed bgImg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      fredericka: ['Fredericka the Great', 'cursive'],
+      tenor: ['Tenor Sans', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
