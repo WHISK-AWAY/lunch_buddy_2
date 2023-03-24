f2e447487aebb8dc60119e1bb884131443dd764b
Andrew <andrewjacobalvarez22@gmail.com>
2023-03-24 09:31:20 -0500

    chatbox working

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,61 +1,84 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { io } from 'socket.io-client';
+import {
+  getActiveMeeting,
+  addMessageActiveMeeting,
+} from '../redux/slices/meetingSlice';
 
 const socket = io.connect('http://localhost:5000');
 
 export default function ChatBox() {
-  const [messageBox, setMessageBox] = useState('');
-  // only in a state for testing purposes
-  const [roomVal, setRoomVal] = useState('12345');
-  // currently the room is hard coded but I'd like it to be a mixture of the meeting id user id and buddy id or something of that sort
+  const dispatch = useDispatch();
+  const [newMessage, setNewMessage] = useState('');
+  //   const [meeting, setMeeting] = useState('');
+  // currently the room is hard coded but I'd like it to be a mixture of the meeting id user id and buddy id or something of that sor
+  const meeting = useSelector((state) => state.meetings.meeting);
+  //   setMeeting(meetingState);
 
-  const addnewLine = (message) => {
-    const div = document.createElement('div');
-    div.textContent = message;
-    document.getElementById('chatbox').appendChild(div);
-  };
   useEffect(() => {
-    // JOIN PROPER ROOM ON LOADUP WILL BE DONE WITH SLICE/AXIOS CALL TO GET PROPER MEETING DATA
-    socket.emit('joinRoom', roomVal);
+    // if (meeting.messages === undefined) {
+    const asyncStart = async () => {
+      const token = localStorage.getItem('token');
+      const disMeeting = await dispatch(getActiveMeeting(token));
+      socket.emit('joinRoom', disMeeting.payload.id);
+    };
+    asyncStart();
+    // }
   }, []);
 
   useEffect(() => {
     // REMOVE .OFF WHEN DEPLOYING OR ELSE WILL NEVER SEND MSG
-    socket.off('recieve-message').on('recieve-message', (message) => {
-      addnewLine(message);
+    socket.off('recieve-message').on('recieve-message', (d) => {
+      const asyncEvent = async () => {
+        console.log('get it');
+        const token = localStorage.getItem('token');
+        setTimeout(() => {
+          dispatch(getActiveMeeting(token));
+        }, 500);
+      };
+      asyncEvent();
     });
   }, [socket]);
 
-  const onMessageSubmit = (e) => {
+  const onMessageSubmit = async (e) => {
     e.preventDefault();
-    if (messageBox === 'join') return;
+    if (newMessage === '') return;
     else {
-      socket.emit('message-event', messageBox, roomVal, 'bob');
-      addnewLine(messageBox);
-      setMessageBox('');
+      const token = localStorage.getItem('token');
+      dispatch(addMessageActiveMeeting({ token, newMessage }));
+      socket.emit('message-event', meeting.id);
+      setNewMessage('');
     }
   };
   return (
     <div className="block">
-      <div className="border-8 border-black m-5 p-5" id="chatbox"></div>
+      <div className="border-8 border-black m-5 p-5" id="chatbox">
+        {!meeting.messages ? (
+          <div>No Messages</div>
+        ) : (
+          <div>
+            {meeting?.messages?.map((message) => {
+              return (
+                <div key={message.id}>
+                  {message.senderId}: {message.message}
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
       <form id="form">
         <label>message</label>
         <input
           type="text"
           className="border border-black"
-          value={messageBox}
-          onChange={(e) => setMessageBox(e.target.value)}
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
         />
         <button type="submit" className="p-10" onClick={onMessageSubmit}>
           Send
         </button>
-        <label htmlFor="">room</label>
-        <input
-          className="border border-black"
-          value={roomVal}
-          onChange={(e) => setRoomVal(e.target.value)}
-        />
-        <button type="submit">join</button>
       </form>
     </div>
   );
