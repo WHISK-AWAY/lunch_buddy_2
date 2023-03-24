d23d340cea96bdeee31d155ad6ea8c18f4fa0441
ruralFluffyDill <stacylukavsky@gmail.com>
2023-03-23 23:52:39 -0400

    color fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        headers: '#365314',
+        headers: '#F68080',
         'primary-gray': '#4A4B51',
         label: '#F68080',
         'light-gray': 'rgba(196, 196, 196, 12%)',
