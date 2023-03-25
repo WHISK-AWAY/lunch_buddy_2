a0fa65d1f0b75f6f75462bb40d988834e9bbc03e
inathan44 <inathan44@yahoo.com>
2023-03-25 16:08:08 -0400

    Updated styling for messages page

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -7,11 +7,12 @@ import {
   addMessageActiveMeeting,
 } from '../redux/slices/meetingSlice';
 
-const socket = io.connect('http://localhost:5000');
+const socket = io.connect('http://localhost:3333');
 
 export default function ChatBox() {
   const dispatch = useDispatch();
   const [newMessage, setNewMessage] = useState('');
+
   //   const [meeting, setMeeting] = useState('');
   // currently the room is hard coded but I'd like it to be a mixture of the meeting id user id and buddy id or something of that sor
   const meeting = useSelector((state) => state.meetings.meeting);
@@ -31,6 +32,10 @@ export default function ChatBox() {
     };
     asyncStart();
     // }
+    setTimeout(() => {
+      const scrollAnchor = document.getElementById('scroll-here');
+      scrollAnchor?.scrollIntoView();
+    }, 500);
   }, []);
 
   useEffect(() => {
@@ -41,6 +46,10 @@ export default function ChatBox() {
         setTimeout(() => {
           dispatch(getActiveMeeting(token));
         }, 500);
+        setTimeout(() => {
+          const scrollAnchor = document.getElementById('scroll-here');
+          scrollAnchor.scrollIntoView();
+        }, 1000);
       };
       asyncEvent();
     });
@@ -56,66 +65,81 @@ export default function ChatBox() {
       setNewMessage('');
     }
   };
+
+  const handleEnterClick = (e) => {
+    if (e.key === 'Enter' && !e.shiftKey) {
+      onMessageSubmit(e);
+    }
+  };
+
   return (
     <div className="flex overflow-hidden h-[calc(100vh_-_48px)] orange-linear-bg">
-      <img
-        src="src/assets/bgImg/chatView.jpg"
-        alt="Two People eating a bowl of food with chopsticks!"
-        className="w-1/2 hidden lg:block"
-      />
-      <div className="flex flex-col h-[calc(100vh_-_48px)] lg:w-1/2 ">
-        <div className="">
-          <h2 className="text-center font-tenor pt-4">Buddy</h2>
-          <h2 className="text-center text-gray-500">
-            {monthToday}-{dayOfMonth}-{yearToday}
-          </h2>
-        </div>
-        <div className=" grow m-1 p-5 overflow-scroll h-3/5 w-fit   ">
-          {!meeting.messages ? (
-            <div>No Messages</div>
-          ) : (
-            <div>
-              {meeting?.messages?.map((message) => {
-                return (
-                  <div key={message.id} className="flex">
-                    {message.senderId === 1 && (
-                      <img
-                        className="self-center mr-3 w-10 h-10 rounded-full"
-                        src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-                      ></img>
-                    )}
-                    <p
-                      className={` py-2 px-4  min-w-[60px] text-center my-2  break-words font-poppins  font-light  text-sm ${
-                        message.senderId === 8
-                          ? 'bg-sender-message ml-auto text-white rounded-l-full rounded-tr-full  '
-                          : 'bg-buddy-message rounded-r-full rounded-tl-full '
-                      }`}
-                    >
-                      {message.message}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-          )}
+      <div className="lg:w-2/5">
+        <img
+          src="src/assets/bgImg/chatView.jpg"
+          alt="Two People eating a bowl of food with chopsticks!"
+          className="hidden lg:block object-cover h-full w-full"
+        />
+      </div>
+      <div className="flex flex-col h-[calc(100vh_-_72px)] w-full lg:w-3/5 ">
+        {/* GREY BOX THAT HOLDS MESSAGES */}
+        <div className="lg:bg-[#c4c4c4] lg:bg-opacity-20 lg:h-full lg:rounded-xl lg:m-8 overflow-y-auto grow">
+          <div className="">
+            <h2 className="text-center font-tenor pt-4 text-lg">Buddy</h2>
+            <h2 className="text-center text-gray-500">
+              {monthToday}-{dayOfMonth}-{yearToday}
+            </h2>
+          </div>
+          <div className="grow m-1 p-5 overflow-y-auto">
+            {!meeting.messages ? (
+              <div className="text-center">No Messages</div>
+            ) : (
+              <>
+                <div>
+                  {meeting?.messages?.map((message) => {
+                    return (
+                      <div key={message.id} className="flex">
+                        {message.senderId === 1 && (
+                          <img
+                            className="self-center mr-3 w-10 h-10 rounded-full"
+                            src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80"
+                          ></img>
+                        )}
+                        <p
+                          className={` py-2 px-4  min-w-[60px] text-center my-2  break-words font-poppins  font-light  text-sm ${
+                            message.senderId === 8
+                              ? 'bg-sender-message ml-auto text-white rounded-l-full rounded-tr-full  '
+                              : 'bg-buddy-message rounded-r-full rounded-tl-full '
+                          }`}
+                        >
+                          {message.message}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+                {/* ANCHOR MESSAGE SCROLL TO BOTTOM */}
+                <div id="scroll-here"></div>
+              </>
+            )}
+          </div>
         </div>
         <form
           id="form"
-          className="w-11/12  self-center mb-2 flex gap-3 bg-transparent"
+          className="w-11/12  self-center mb-2 lg:flex gap-3 bg-transparent"
         >
           <textarea
             type="text"
             className=" border border-black w-full rounded-2xl h-16 py-2 px-6"
             value={newMessage}
             onChange={(e) => setNewMessage(e.target.value)}
+            onKeyDown={handleEnterClick}
           />
           <button
             type="submit"
             className=" hover:bg-orange-100 px-3 rounded-lg transition-all ease-in-out"
             onClick={onMessageSubmit}
-          >
-            <img src={sendIcon} alt="send icon" className="w-6" />
-          </button>
+          ></button>
         </form>
       </div>
     </div>
