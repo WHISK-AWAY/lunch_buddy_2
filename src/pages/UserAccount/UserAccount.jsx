f70103f8256dcd56bfedc8befa5f8b616da79ac1
ruralFluffyDill <stacylukavsky@gmail.com>
2023-03-24 21:42:25 -0400

    style fixes

--- src/pages/UserAccount/UserAccount.jsx
+++ src/pages/UserAccount/UserAccount.jsx
@@ -18,7 +18,6 @@ const UserAccount = () => {
   const [cuisineTags, setCuisineTags] = useState([]);
   const [tagExpand, setTagExpand] = useState(true);
 
-
   useEffect(() => {
     dispatch(tryToken());
   }, []);
@@ -68,7 +67,6 @@ const UserAccount = () => {
     setProfessionalTags(professional || []);
     setDietaryTags(dietary || []);
     setCuisineTags(cuisine || []);
-
   }, [user]);
 
   console.log('social', socialTags);
@@ -76,12 +74,15 @@ const UserAccount = () => {
   if (!tags) return <p>Loading tags...</p>;
 
   return (
-    <div id="user-container font-tenor">
-      <div className="flex flex-col items-center justify-center w-screen mx-2 px-4">
+    <div
+      id="user-container"
+      className="font-tenor flex flex-row-reverse flex-nowrap w-screen justify-center h-[calc(100vh_-_69px)] lg:overflow-hidden"
+    >
+      <div className="lg:basis-1/2 basis-4/5 flex flex-col items-center mx-2 px-4 h-full lg:overflow-auto md:px-10">
         <h1 className="my-8 text-2xl text-headers">
           {auth.user.fullName.toUpperCase()}
         </h1>
-        <div id="user-avatar" className=" flex justify-center w-96">
+        <div id="user-avatar" className=" flex justify-center ">
           <img
             src={auth.user.avatarUrl}
             alt="user avatar"
@@ -89,37 +90,38 @@ const UserAccount = () => {
           />
         </div>
         {/*Link to edit page */}
-        <div id="about-me" className="pt-14">
+        <div id="about-me" className="pt-14 text-justify text-primary-gray">
           <p>{auth.user.aboutMe}</p>
         </div>
 
-        <div id="tags-container" className="flex flex-wrap px-10 my-8">
-          <h2 className="ml-12">
+        <div id="tags-container" className="flex flex-wrap my-8">
+          <h2 className="">
             {socialTags[0]?.category.categoryName.toUpperCase()}
           </h2>
 
-          <div
-            id="social-tags"
-            className="w-full flex flex-wrap gap-x-5 gap-y-2 my-6"
-          >
+          <div id="social-tags" className="w-full flex gap-x-5 gap-y-2 my-6">
             <img
-              className={`w-6 transition-all ${tagExpand ? '' : 'rotate-90'}`}
+              className={`w-4 transition-all ${
+                tagExpand ? '' : 'rotate-90'
+              } self-start`}
               src={chevronRight}
               alt="Expand/Retract Arrow"
             />
-            {socialTags.map((social) => {
-              return (
-                <p
-                  key={social.id}
-                  className="border border-black rounded-full px-4 h-7 lg:h-auto flex  gap-4 items-center"
-                >
-                  {social.tagName}
-                </p>
-              );
-            })}
+            <div className="flex flex-row flex-wrap gap-3">
+              {socialTags.map((social) => {
+                return (
+                  <p
+                    key={social.id}
+                    className="border border-primary-gray rounded-full px-4 h-7 lg:h-auto flex  gap-4 items-center justify-center grow"
+                  >
+                    {social.tagName}
+                  </p>
+                );
+              })}
+            </div>
           </div>
 
-          <h2 className="ml-12">
+          <h2 className="">
             {professionalTags[0]?.category.categoryName.toUpperCase()}
           </h2>
           <div
@@ -143,7 +145,7 @@ const UserAccount = () => {
             })}
           </div>
 
-          <h2 className="ml-12">
+          <h2 className="">
             {cuisineTags[0]?.category.categoryName.toUpperCase()}
           </h2>
           <div
@@ -194,6 +196,10 @@ const UserAccount = () => {
           )}
         </div>
       </div>
+      <div
+        className="bg-cover
+          bg-[url('/assets/bgImg/accView.jpg')] basis-1/2 hidden lg:block h-full"
+      ></div>
     </div>
   );
 };
