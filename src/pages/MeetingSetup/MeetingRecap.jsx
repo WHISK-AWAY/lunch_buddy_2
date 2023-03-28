45899804ca4131d14780cc0ca3504072e57f11e3
Patrick Bryan <pbryan9@gmail.com>
2023-03-27 19:04:44 -0700

    meeting recap styling complete

--- src/pages/MeetingSetup/MeetingRecap.jsx
+++ src/pages/MeetingSetup/MeetingRecap.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { createMeeting } from '../../redux/slices';
+import {
+  createMeeting,
+  selectMeetings,
+  resetMeetingStatus,
+} from '../../redux/slices';
 
 export default function MeetingRecap(props) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const meetings = useSelector(selectMeetings);
+
   const location = useLocation();
+
+  useEffect(() => {
+    // on load, make sure meeting state is cleared
+    dispatch(resetMeetingStatus);
+  }, []);
+
+  useEffect(() => {
+    if (meetings.meeting.id && !meetings.error) navigate('/');
+  }, [dispatch, meetings]);
 
   const { buddy, timeSlot, restaurant } = location.state;
 
@@ -19,42 +34,44 @@ export default function MeetingRecap(props) {
 
   function handleMeeting() {
     dispatch(createMeeting({ newMeeting }));
-    navigate('/');
   }
 
   return (
-    <div className="recap-card orange-linear-bg bg-fixed h-[calc(100vh_-_69px)] pt-16 text-primary-gray flex flex-col gap-12 items-center">
-      <div className="recap-header text-headers text-xl font-semibold">
-        <h1>MEETING RECAP</h1>
-      </div>
-      <div className="recap-body flex flex-col items-center">
-        <div className="buddy-avatar-container rounded-full mb-6">
-          <img
-            src={buddy.avatarUrl}
-            alt="Your buddy's avatar image"
-            className="object-cover w-16 h-16 rounded-full"
-          />
+    <div className="recap-card h-[calc(100vh_-_75px)] w-screen flex flex-col gap-12 items-center orange-linear-bg lg:bg-none lg:bg-white lg:flex-row lg:items-start bg-fixed text-primary-gray overflow-hidden">
+      <div className="recap-image hidden bg-left lg:block lg:h-full lg:basis-1/2 bg-[url('/assets/bgImg/meetingConfView.jpg')] bg-cover overflow-hidden"></div>
+      <div className="recap-info flex flex-col basis-full h-full pt-16 lg:basis-1/2 gap-12 items-center overflow-auto">
+        <div className="recap-header text-headers text-xl font-semibold">
+          <h1>MEETING RECAP</h1>
         </div>
-        <h2 className="text-lg font-semibold">
-          {buddy.fullName.toUpperCase()}
-        </h2>
-        <p>
-          {timeSlot.startTime} - {timeSlot.endTime}
-        </p>
-        <p className="font-semibold">
-          <a href={restaurant.url} target="_blank">
-            {restaurant.name.toUpperCase()}
-          </a>
-        </p>
-        <p>{restaurant.location?.display_address?.join(' ')}</p>
-      </div>
-      <div className="recap-button">
-        <button
-          className="button rounded-full text-white"
-          onClick={(e) => handleMeeting(e)}
-        >
-          Letsfkngoooo
-        </button>
+        <div className="recap-body flex flex-col items-center">
+          <div className="buddy-avatar-container rounded-full mb-6">
+            <img
+              src={buddy.avatarUrl}
+              alt="Your buddy's avatar image"
+              className="bg-white object-cover aspect-square w-32 h-32 rounded-full z-10 p-1 drop-shadow-md"
+            />
+          </div>
+          <h2 className="text-lg font-semibold">
+            {buddy.fullName.toUpperCase()}
+          </h2>
+          <p>
+            {timeSlot.startTime} - {timeSlot.endTime}
+          </p>
+          <p className="font-semibold">
+            <a href={restaurant.url} target="_blank">
+              {restaurant.name.toUpperCase()}
+            </a>
+          </p>
+          <p>{restaurant.location?.display_address?.join(' ')}</p>
+        </div>
+        <div className="recap-button flex flex-col items-center w-4/5">
+          <button
+            className="px-5 py-2 w-11/12 lg:py-2 rounded-full button text-white text-md"
+            onClick={(e) => handleMeeting(e)}
+          >
+            INVITE BUDDY
+          </button>
+        </div>
       </div>
     </div>
   );
