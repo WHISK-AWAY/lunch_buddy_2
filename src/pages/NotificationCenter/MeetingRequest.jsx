f22fddafc36a41950c2beb74d7d88fcd73925181
Patrick Bryan <pbryan9@gmail.com>
2023-03-31 10:52:33 -0700

    add feedback msg following notification interaction

--- src/pages/NotificationCenter/MeetingRequest.jsx
+++ src/pages/NotificationCenter/MeetingRequest.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigate } from 'react-router-dom';
 import { getMeeting, selectMeetings } from '../../redux/slices';
 import {
   getBusinessInfo,
@@ -12,12 +12,15 @@ import { fetchAllNotifications } from '../../redux/slices';
 import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
+const POST_NOTIFICATION_TIMEOUT = 5000;
+
 export default function MeetingRequest({ notification }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const meetings = useSelector(selectMeetings);
 
   const token = window.localStorage.getItem('token');
-  const [accepted, setAccepted] = useState(false);
+  const [response, setResponse] = useState(null);
 
   useEffect(() => {
     dispatch(
@@ -26,8 +29,8 @@ export default function MeetingRequest({ notification }) {
         userId: notification.toUser.id,
       })
     );
-    // if (notification.meeting.yelpBusinessId)
-    // dispatch(getBusinessInfo(notification.meeting.yelpBusinessId));
+    if (notification.meeting.yelpBusinessId)
+      dispatch(getBusinessInfo(notification.meeting.yelpBusinessId));
   }, [notification]);
 
   const yelpBusinessId = notification.meeting.yelpBusinessId;
@@ -36,80 +39,171 @@ export default function MeetingRequest({ notification }) {
 
   console.log('meeting', meetings.business);
 
-  const handleAccept = async () => {
-    dispatch(
-      updateNotificationStatus({
-        userId: notification.toUserId,
-        notificationId: notification.id,
-        updates: { isAcknowledged: true },
-      })
-    );
-
-    const res = await axios.put(
-      API_URL +
-        `/api/user/${notification.toUser.id}/meeting/${notification.meeting.id}/confirm`,
-      {},
-      { headers: { authorization: token } }
-    );
-
+  const localRedirect = (linkTo) => {
+    navigate(linkTo);
     setTimeout(() => {
       dispatch(fetchAllNotifications({ userId: notification.toUser.id }));
     }, 500);
-    // console.log('userID', notification.userId);
-    // console.log('meeetingID', notification.meetingId);
+  };
+
+  const handleAccept = async () => {
+    setResponse('accepted');
+
+    setTimeout(async () => {
+      dispatch(
+        updateNotificationStatus({
+          userId: notification.toUserId,
+          notificationId: notification.id,
+          updates: { isAcknowledged: true },
+        })
+      );
+
+      await axios.put(
+        API_URL +
+          `/api/user/${notification.toUser.id}/meeting/${notification.meeting.id}/confirm`,
+        {},
+        { headers: { authorization: token } }
+      );
+
+      setTimeout(() => {
+        dispatch(fetchAllNotifications({ userId: notification.toUser.id }));
+      }, 500);
+    }, POST_NOTIFICATION_TIMEOUT);
   };
 
   const handleReject = () => {
-    dispatch(
-      updateNotificationStatus({
-        userId: notification.toUserId,
-        notificationId: notification.id,
-        updates: { isAcknowledged: true },
-      })
-    );
+    setResponse('rejected');
 
-    dispatch(
-      cancelMeeting({
-        userId: notification.toUserId,
-        meetingId: notification.meetingId,
-      })
-    );
+    setTimeout(() => {
+      dispatch(
+        updateNotificationStatus({
+          userId: notification.toUserId,
+          notificationId: notification.id,
+          updates: { isAcknowledged: true },
+        })
+      );
+
+      dispatch(
+        cancelMeeting({
+          userId: notification.toUserId,
+          meetingId: notification.meetingId,
+        })
+      );
+
+      setTimeout(() => {
+        dispatch(fetchAllNotifications({ userId: notification.toUser.id }));
+      }, 500);
+    }, POST_NOTIFICATION_TIMEOUT);
   };
 
-  // console.log('not', notification);
+  // Render one thing before accept/reject, render another thing after
   return (
-    <div
-      id="meeting-card"
-      className="flex w-full h-fit bg-gray-100/90 rounded-2xl drop-shadow-sm my-3 items-center justify-between "
-    >
-      <div id="img-section" className="px-2 h-28 shrink-0">
-        <img
-          src={notification.fromUser.avatarUrl}
-          alt="user avatar"
-          className="object-cover  aspect-square w-20 h-20 rounded-[100%] z-10 bg-white p-1  drop-shadow-lg relative translate-y-[30%] "
-        />
-      </div>
-      <div
-        id="notification-details"
-        className="flex flex-col self-center text-center text-xs w-full py-2"
-      >
-        <p className="pb-2">new buddy wants to connect</p>
-        <p>{notification.fromUser.fullName.toUpperCase()}</p>
-        <p>{new Date(notification.meeting.lunchDate).toLocaleString()}</p>
-        <p>
-          {notification.meeting?.yelpBusinessId &&
-            meetings.business[yelpBusinessId]?.name}
-        </p>
-        <p>{notification.meeting?.yelpBusinessId && yelpBusinessAddress}</p>
+    <React.Fragment>
+      {response === null && (
         <div
-          id="btn-container"
-          className="flex flex-row gap-2 w-fit h-fit self-center text-xs space-5 justify-center items-center"
+          id="meeting-card"
+          className="flex w-full h-fit bg-gray-100/90 rounded-2xl drop-shadow-sm my-3 items-center justify-between "
         >
-          <FormButton handleSubmit={handleAccept}>ACCEPT </FormButton>
-          <FormButton handleSubmit={handleReject}>REJECT </FormButton>
+          <div id="img-section" className="px-2 h-28 shrink-0">
+            <img
+              src={notification.fromUser.avatarUrl}
+              alt="user avatar"
+              className="object-cover  aspect-square w-20 h-20 rounded-[100%] z-10 bg-white p-1  drop-shadow-lg relative translate-y-[30%] "
+            />
+          </div>
+          <div
+            id="notification-details"
+            className="flex flex-col self-center text-center text-xs w-full py-2"
+          >
+            <p className="pb-2">new buddy wants to connect</p>
+            <p>{notification.fromUser.fullName.toUpperCase()}</p>
+            <p>{new Date(notification.meeting.lunchDate).toLocaleString()}</p>
+            <p>
+              {notification.meeting?.yelpBusinessId &&
+                meetings.business[yelpBusinessId]?.name}
+            </p>
+            <p>{notification.meeting?.yelpBusinessId && yelpBusinessAddress}</p>
+            <div
+              id="btn-container"
+              className="flex flex-row gap-2 w-fit h-fit self-center text-xs space-5 justify-center items-center"
+            >
+              <FormButton handleSubmit={handleAccept}>ACCEPT </FormButton>
+              <FormButton handleSubmit={handleReject}>REJECT </FormButton>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+      {response === 'accepted' && (
+        <div
+          id="meeting-card"
+          className="flex w-full h-fit bg-gray-100/90 rounded-2xl drop-shadow-sm my-3 items-center justify-between "
+        >
+          <div id="img-section" className="px-2 h-28 shrink-0">
+            <img
+              src={notification.fromUser.avatarUrl}
+              alt="user avatar"
+              className="object-cover  aspect-square w-20 h-20 rounded-[100%] z-10 bg-white p-1  drop-shadow-lg relative translate-y-[30%] "
+            />
+          </div>
+          <div
+            id="notification-details"
+            className="flex flex-col self-center text-center text-base w-full py-2"
+          >
+            <p className="pb-2">
+              ok! we'll let {notification.fromUser.firstName} know you're in!
+            </p>
+
+            {notification.meeting?.yelpBusinessId && (
+              <p>
+                Meanwhile, check out{' '}
+                <a
+                  target="_blank"
+                  href={meetings.business[yelpBusinessId]?.url}
+                  className="text-headers"
+                >
+                  {meetings.business[yelpBusinessId]?.name}
+                </a>
+                !
+              </p>
+            )}
+            {/* <div
+              id="btn-container"
+              className="flex flex-row gap-2 w-fit h-fit self-center text-xs space-5 justify-center items-center"
+            >
+              <FormButton handleSubmit={}>HOME</FormButton>
+              <FormButton handleSubmit={}>MESSAGE {notification.fromUser.firstName}</FormButton>
+            </div> */}
+          </div>
+        </div>
+      )}
+      {response === 'rejected' && (
+        <div
+          id="meeting-card"
+          className="flex w-full h-fit bg-gray-100/90 rounded-2xl drop-shadow-sm my-3 items-center justify-between "
+        >
+          <div
+            id="notification-details"
+            className="flex flex-col self-center text-center text-xs w-full py-2"
+          >
+            <p className="pb-2 text-base">
+              too bad -- we'll break it to {notification.fromUser.firstName}{' '}
+              gently...
+            </p>
+            <div
+              id="btn-container"
+              className="flex flex-row gap-2 w-fit h-fit self-center text-xs space-5 justify-center items-center"
+            >
+              <FormButton handleSubmit={() => localRedirect('/')}>
+                HOME
+              </FormButton>
+              <FormButton handleSubmit={() => localRedirect('/match')}>
+                FIND A BUDDY
+              </FormButton>
+            </div>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
