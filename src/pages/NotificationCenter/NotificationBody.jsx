04c869dec40e3c24fc052c14276b535434df2250
ruralFluffyDill <stacylukavsky@gmail.com>
2023-03-31 22:06:22 -0400

    transition test

--- src/pages/NotificationCenter/NotificationBody.jsx
+++ src/pages/NotificationCenter/NotificationBody.jsx
@@ -10,29 +10,46 @@ import RatingRequest from './RatingRequest';
 // import MeetingRejected from './MeetingRejected';
 // import RatingRequest from './RatingRequest';
 
-const NotificationBody = () => {
+const NotificationBody = ({
+  showNotificationBody,
+  setShowNotificationBody,
+}) => {
   const notifications = useSelector(selectUnreadNotifications);
 
-  // const notificationType = notifications.
+  // function handleClick() {
+  //   setExpandMenu(false);
+  // }
+
+  console.log('body', showNotificationBody);
+
   if (!notifications?.length) return <h1>No notifications to render...</h1>;
-  // console.log('hey');
+
   return (
-    <div className="flex flex-col">
-      <div className="absolute z-40 bg-primary-gray/60 self-end lg:h-[46rem] lg:w-[35rem] rounded-l-3xl px-3 py-3">
-        <ul>
-          {notifications?.map((notification) => {
-            return (
-              <li key={notification.id}>
-                {notification.notificationType === 'meetingInvite' && (
-                  <MeetingRequest notification={notification} />
-                )}
-                {notification.notificationType === 'ratingRequested' && (
-                  <RatingRequest notification={notification} />
-                )}
-              </li>
-            );
-          })}
-        </ul>
+    <div
+      id="nofification-container"
+      className={showNotificationBody ? '' : 'group hover'}
+    >
+      <div
+        className={`transform group-[.hover]:scale-0 scale-100 h-fit overflow:hidden absolute transition-transform duration-[300ms] ease-in-out origin-top-right -bottom-screen opacity-95 right-0 transition-opacity-0`}
+      >
+        <div className="flex flex-col">
+          <div className=" z-40 bg-primary-gray/60 self-end h-fit lg:w-[35rem] rounded-l-3xl px-3 py-3 w-3/5">
+            <ul>
+              {notifications?.map((notification) => {
+                return (
+                  <li key={notification.id}>
+                    {notification.notificationType === 'meetingInvite' && (
+                      <MeetingRequest notification={notification} />
+                    )}
+                    {notification.notificationType === 'ratingRequested' && (
+                      <RatingRequest notification={notification} />
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
