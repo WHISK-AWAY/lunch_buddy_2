f2c365c56a83d8e753e11a82694ecaccfe50f8e0
ruralFluffyDill <stacylukavsky@gmail.com>
2023-04-01 11:14:31 -0400

    rating req styling

--- src/pages/NotificationCenter/RatingRequest.jsx
+++ src/pages/NotificationCenter/RatingRequest.jsx
@@ -44,10 +44,10 @@ export default function RatingRequest({ notification }) {
   return (
     <div
       id="rating-request-card"
-      className="flex flex-col w-full h-fit bg-gray-100/90 rounded-2xl drop-shadow-sm my-3 items-center justify-between py-5"
+      className="flex flex-col w-full h-34 bg-gray-100/90 rounded-2xl drop-shadow-sm my-3 items-center justify-between py-5"
     >
       <div id="notification-details">
-        <p>
+        <p className="px-2 text-center">
           Tell us about your meeting with{' '}
           <span>{notification.fromUser.firstName}</span>
         </p>
