461fcd270eca7b2ccdaf5610a90b60026a228d92
ruralFluffyDill <stacylukavsky@gmail.com>
2023-04-02 17:58:08 -0400

    fixed uppercase and layout

--- src/pages/aboutYourself/Bio.jsx
+++ src/pages/aboutYourself/Bio.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 
 const Bio = ({ setBio, bio }) => {
   return (
-    <div className="relative mb-9 w-4/5 lg:w-5/12">
+    <div className="flex relative mb-9 lg:w-9/12 w-10/12 lg:basis-1/2 text-primary-gray self-center  h-[calc(100vh_-_65px)]">
       <label className="text-label block text-xs absolute -top-3 left-3 bg-white px-1">
         Bio
       </label>
       <textarea
-        className="w-full px-4 rounded-2xl focus:outline-none border border-primary-gray h-24 py-4"
+        type="text"
+        rows="24"
+        className="w-full px-4 rounded-2xl focus:outline-none border resize-none h-24 border-primary-gray text-sm py-2 scrollbar-hide"
         onChange={(e) => setBio(e.target.value)}
         value={bio}
       />
