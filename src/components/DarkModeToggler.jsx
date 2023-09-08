a5a8de742e00b0c3b01d3c100e04cecbcaa7f85b
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-08 14:47:07 -0400

    add selector DM logic

--- src/components/DarkModeToggler.jsx
+++ src/components/DarkModeToggler.jsx
@@ -1,37 +1,47 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  selectDarkMode,
+  darkModeOn,
+  darkModeOff,
+} from '../redux/slices/darkModeSlice';
 
-export default function DarkModeToggler({isDarkMode, setIsDarkMode}) {
+export default function DarkModeToggler({ isDarkMode, setIsDarkMode }) {
+  const dispatch = useDispatch();
 
+  const darkModeSelector = useSelector(selectDarkMode);
 
+  // console.log(darkModeSelector);
+  useEffect(() => {
+    if (!isDarkMode) dispatch(darkModeOff());
+    else dispatch(darkModeOn());
+  }, [isDarkMode]);
 
   const toggleDarkMode = () => {
     setIsDarkMode(!isDarkMode);
   };
 
-
-   useEffect(() => {
-     if (isDarkMode) {
+  useEffect(() => {
+    if (isDarkMode) {
       document.documentElement.classList.add('dark');
     } else {
-       document.documentElement.classList.remove('dark');
-     }
-   }, [isDarkMode]);
+      document.documentElement.classList.remove('dark');
+    }
+  }, [isDarkMode]);
 
   return (
-  
-      <label className="flex absolute top-0 right-1/2 items-center cursor-pointer">
-        <span className="mr-2 text-gray-600 dark:text-gray-300">dark mode</span>
-        <div className="relative">
-          <input
-            type="checkbox"
-            className="hidden"
-            onChange={toggleDarkMode}
-            checked={isDarkMode}
-          />
-          <div className=" w-10 h-4 bg-gray-400 rounded-full shadow-inner dark:bg-gray-700"></div>
-          <div className=" absolute w-6 h-6 bg-white border-2 border-gray-400 rounded-full shadow inset-y-0 left-0"></div>
-        </div>
-      </label>
-    
+    <label className="flex absolute top-0 right-1/2 items-center cursor-pointer">
+      <span className="mr-2 text-gray-600 dark:text-gray-300">dark mode</span>
+      <div className="relative">
+        <input
+          type="checkbox"
+          className="hidden"
+          onChange={toggleDarkMode}
+          checked={isDarkMode}
+        />
+        <div className=" w-10 h-4 bg-gray-400 rounded-full shadow-inner dark:bg-gray-700"></div>
+        <div className=" absolute w-6 h-6 bg-white border-2 border-gray-400 rounded-full shadow inset-y-0 left-0"></div>
+      </div>
+    </label>
   );
 }
