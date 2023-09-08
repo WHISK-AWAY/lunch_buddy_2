e4a1835b6af894ac6f5df9aa25b5a3b92b4a2ebf
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-07 23:23:44 -0400

    add resp sdtyling

--- src/pages/Feedback/Rating.jsx
+++ src/pages/Feedback/Rating.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 const Rating = ({ starRating, setStarRating, noRating }) => {
   return (
-    <div className="font-tenor border rounded-3xl flex flex-col justify-center text-primary-gray items-center border-primary-gray w-11/12 mx-auto gap-y-6 px-4 py-8">
-      <h2 className="text-headers text-lg">WE CARE ABOUT YOUR FEEDBACK</h2>
-      <p>how was it?</p>
+    <div className=" border rounded-lg flex flex-col justify-center text-primary-gray dark:text-white items-center border-primary-gray w-11/12 mx-auto gap-y-6 px-4 py-8 lg:w-4/5 2xl:w-3/5 5xl:w-2/5">
+      <h2 className="text-headers text-lg md:text-sm">WE CARE ABOUT YOUR FEEDBACK</h2>
+      <p className='text-sm'>how was it?</p>
       {noRating && (
         <p className="text-primary-gray text-xs">
           please leave a rating of at least one star.
@@ -16,7 +16,7 @@ const Rating = ({ starRating, setStarRating, noRating }) => {
             <span
               onClick={() => setStarRating(idx + 1)}
               className={`text-3xl ${
-                idx < starRating ? 'text-headers' : 'text-[#ccc]'
+                idx < starRating ? 'text-headers' : 'text-primary-gray'
               }`}
             >
               &#9733;
