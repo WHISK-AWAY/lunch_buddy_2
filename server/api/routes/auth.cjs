b84f8218aadafc74e197556d244db703459ca55f
Patrick Bryan <pbryan9@gmail.com>
2023-09-08 17:36:11 -0700

    implement early email validation

--- server/api/routes/auth.cjs
+++ server/api/routes/auth.cjs
@@ -13,6 +13,28 @@ router.get('/', requireToken, (req, res, next) => {
   }
 });
 
+router.get('/check-email', async (req, res, next) => {
+  // return object containing boolean indicating whether email already exists
+  // intended to be used during registration process for early validation
+
+  // TODO: do checks (as well as registrations) in lowercase
+  try {
+    const { email } = req.query;
+
+    if (!email)
+      return res
+        .status(400)
+        .json({ message: 'Error: no e-mail address specified' });
+
+    const emailCheck = await User.findOne({ where: { email } });
+
+    return res.status(200).json({ emailExists: !!emailCheck });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
 router.post('/login', async (req, res, next) => {
   try {
     const { email, password } = req.body;
