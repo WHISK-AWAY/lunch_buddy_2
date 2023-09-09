bfa4946679c6c6f7bcf60dbd7652ce7ff80dd154
Patrick Bryan <pbryan9@gmail.com>
2023-09-09 12:35:09 -0700

    rework menu animation

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -22,7 +22,8 @@ const DropdownMenu = ({ menuMode, navHeight, closeMenu }) => {
   const dispatch = useDispatch();
 
   const wrapperRef = useRef(null);
-  const tlRef = useRef(null);
+  const screenRef = useRef(null);
+  const previousModeRef = useRef(null);
 
   const authUser = useSelector(selectAuthUser);
   const notifications = useSelector(selectUnreadNotifications);
@@ -33,24 +34,53 @@ const DropdownMenu = ({ menuMode, navHeight, closeMenu }) => {
 
   useEffect(() => {
     if (menuMode === 'dropdown') {
+      previousModeRef.current = 'dropdown';
       const ctx = gsap.context(() => {
-        const tl = gsap.timeline();
-        tlRef.current = tl;
+        const tl = gsap.timeline({});
 
-        tl.to(wrapperRef.current, {
-          y: navHeight,
-          duration: 0.5,
-        });
+        tl.set(screenRef.current, { display: 'block' })
+          .to(wrapperRef.current, {
+            y: '+=100%',
+            duration: 0.25,
+            ease: 'power1.in',
+          })
+          .to(
+            screenRef.current,
+            {
+              backdropFilter: 'blur(8px)',
+              duration: 0.25,
+            },
+            '<'
+          );
       });
 
-      return () => {
-        if (tlRef.current) {
-          tlRef.current.reverse().then(() => {
-            tlRef.current = null;
-            ctx.revert();
-          });
-        } else ctx.revert();
-      };
+      return () => ctx.revert();
+    } else {
+      if (previousModeRef.current === 'dropdown') {
+        const ctx = gsap.context(() => {
+          const tl = gsap.timeline();
+
+          tl.from(wrapperRef.current, {
+            y: '+=100%',
+            ease: 'power1.in',
+            duration: 0.25,
+          })
+            .from(
+              screenRef.current,
+              {
+                backdropFilter: 'blur(8px)',
+                display: 'block',
+                duration: 0.25,
+              },
+              '<'
+            )
+            .set(screenRef.current, { display: 'none' });
+        });
+
+        previousModeRef.current = null;
+
+        return () => ctx.revert();
+      }
     }
   }, [menuMode, wrapperRef.current]);
 
@@ -104,97 +134,78 @@ const DropdownMenu = ({ menuMode, navHeight, closeMenu }) => {
         console.log('user denied location services permission:', err);
       }
     );
-
-    // setTimeout(async () => {
-    //   const center = {
-    //     latitude: userState.lastLat,
-    //     longitude: userState.lastLong,
-    //   };
-
-    //   await axios.post(
-    //     API_URL + '/api/user/generate/demo',
-    //     {
-    //       center,
-    //       radius: 1,
-    //       city: userState.city,
-    //       state: userState.state,
-    //       id: userState.id,
-    //     },
-    //     {
-    //       headers: {
-    //         Authorization: window.localStorage.getItem('token'),
-    //       },
-    //     }
-    //   );
-    //   console.log('userState:', userState);
-    //   navigate('/match');
-    // }, 5000);
-    // set status to active
-    // call the demo mode route
-    // maybe a demo mode toast?
   }
 
-  //prevent scroll on overflow when the menu is open
-  // useEffect(() => {
-  //   if (expandMenu) {
-  //     document.body.style.overflow = 'hidden';
-  //     return () => {
-  //       document.body.style.overflow = '';
-  //     };
-  //   }
-  // }, []);
+  /**
+   * Removed from wrapper class list:
+   *
+   * top-0
+   */
+
   return (
-    <div
-      ref={wrapperRef}
-      id="dropdown-container"
-      className="overflow:hidden dark:text-white dark:bg-[#0a0908] -translate-y-full absolute top-0 bg-white w-screen opacity-95 z-50"
-    >
-      <ul className="flex flex-col items-center ">
-        {!authUser.firstName ? (
-          <>
-            {/* NAV LINKS WHEN NOT SIGNED IN */}
-            <DropDownItem handleClick={handleClick} linkTo="/register">
-              SIGN UP
-            </DropDownItem>
-            <DropDownItem handleClick={handleClick} linkTo="/login">
-              LOG IN
-            </DropDownItem>
-            <DropDownItem handleClick={handleClick} linkTo="/">
-              HOME
-            </DropDownItem>
-          </>
-        ) : (
-          <>
-            {/* NAV LINKS WHEN SIGNED IN */}
-            <DropDownItem handleClick={handleClick} linkTo="/">
-              HOME
-            </DropDownItem>
-            <DropDownItem handleClick={handleClick} linkTo="/account">
-              ACCOUNT
-            </DropDownItem>
-            {!currentMeetingNotification?.id ? (
-              <DropDownItem handleClick={handleClick} linkTo="/match">
-                NEW MEETING
+    <>
+      <div
+        ref={screenRef}
+        className="fixed bottom-0 h-screen w-screen backdrop-blur-0 bg-transparent z-20"
+        style={{ display: 'none' }}
+      ></div>
+      <div
+        ref={wrapperRef}
+        id="dropdown-container"
+        className={`dark:text-white dark:bg-[#0a0908] -translate-y-full overflow-clip fixed bg-white w-screen opacity-95 z-30 -top-${navHeight}`}
+      >
+        <ul className="flex flex-col items-center ">
+          {!authUser.firstName ? (
+            <>
+              {/* NAV LINKS WHEN NOT SIGNED IN */}
+              <DropDownItem handleClick={handleClick} linkTo="/register">
+                SIGN UP
               </DropDownItem>
-            ) : (
-              <DropDownItem handleClick={handleClick} linkTo="/meeting/current">
-                CURRENT MEETING
+              <DropDownItem handleClick={handleClick} linkTo="/login">
+                LOG IN
               </DropDownItem>
-            )}
-            {currentMeetingNotification?.id && (
-              <DropDownItem
-                handleClick={handleClick}
-                linkTo={`/meeting/${currentMeetingNotification.meetingId}/chat`}
-              >
-                MESSAGES
+              <DropDownItem handleClick={handleClick} linkTo="/">
+                HOME
               </DropDownItem>
-            )}
-            <DropDownItem handleClick={handleDemoMode}>DEMO MODE</DropDownItem>
-            <DropDownItem handleClick={handleLogout}>LOG OUT</DropDownItem>
-          </>
-        )}
-      </ul>
-    </div>
+            </>
+          ) : (
+            <>
+              {/* NAV LINKS WHEN SIGNED IN */}
+              <DropDownItem handleClick={handleClick} linkTo="/">
+                HOME
+              </DropDownItem>
+              <DropDownItem handleClick={handleClick} linkTo="/account">
+                ACCOUNT
+              </DropDownItem>
+              {!currentMeetingNotification?.id ? (
+                <DropDownItem handleClick={handleClick} linkTo="/match">
+                  NEW MEETING
+                </DropDownItem>
+              ) : (
+                <DropDownItem
+                  handleClick={handleClick}
+                  linkTo="/meeting/current"
+                >
+                  CURRENT MEETING
+                </DropDownItem>
+              )}
+              {currentMeetingNotification?.id && (
+                <DropDownItem
+                  handleClick={handleClick}
+                  linkTo={`/meeting/${currentMeetingNotification.meetingId}/chat`}
+                >
+                  MESSAGES
+                </DropDownItem>
+              )}
+              <DropDownItem handleClick={handleDemoMode}>
+                DEMO MODE
+              </DropDownItem>
+              <DropDownItem handleClick={handleLogout}>LOG OUT</DropDownItem>
+            </>
+          )}
+        </ul>
+      </div>
+    </>
   );
 };
 
--- src/pages/NotificationCenter/NotificationBody.jsx
+++ src/pages/NotificationCenter/NotificationBody.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { selectUnreadNotifications, selectMeetings } from '../../redux/slices';
 
 import MeetingRequest from './MeetingRequest';
 import RatingRequest from './RatingRequest';
 import MeetingRejected from './MeetingRejected';
-import MeetingAccepted from './MeetingAccepted';
 import NewMessageReceived from './NewMessageReceived';
 import CurrentMeeting from './CurrentMeeting';
 import MeetingCancelled from './MeetingCancelled';
-// import RatingRequest from './RatingRequest';
 
 import gsap from 'gsap';
 
@@ -18,107 +15,145 @@ const NotificationBody = ({
   isDarkMode,
   setIsDarkMode,
   menuMode,
-  // notificationBodyIsOpen,
+  closeMenu,
 }) => {
   const wrapperRef = useRef(null);
-  const tlRef = useRef(null);
+  const screenRef = useRef(null);
+  const previousModeRef = useRef(null);
 
   const notifications = useSelector(selectUnreadNotifications);
   const meetings = useSelector(selectMeetings);
 
   useEffect(() => {
     if (menuMode === 'notifications') {
+      previousModeRef.current = 'notifications';
       const ctx = gsap.context(() => {
-        const tl = gsap.timeline();
-        tlRef.current = tl;
+        const tl = gsap.timeline({});
 
-        tl.to(wrapperRef.current, {
-          x: '0%',
-          duration: 0.5,
-        });
+        tl.set(screenRef.current, { display: 'block' })
+          .to(wrapperRef.current, {
+            x: '0%',
+            duration: 0.25,
+            ease: 'power1.in',
+          })
+          .to(
+            screenRef.current,
+            {
+              backdropFilter: 'blur(8px)',
+              duration: 0.25,
+            },
+            '<'
+          );
       });
 
-      return () => {
-        if (tlRef.current) {
-          tlRef.current.reverse().then(() => {
-            tlRef.current = null;
-            ctx.revert();
-          });
-        } else ctx.revert();
-      };
+      return () => ctx.revert();
+    } else {
+      if (previousModeRef.current === 'notifications') {
+        const ctx = gsap.context(() => {
+          const tl = gsap.timeline();
+
+          tl.from(wrapperRef.current, {
+            x: '0%',
+            ease: 'power1.in',
+            duration: 0.25,
+          })
+            .from(
+              screenRef.current,
+              {
+                backdropFilter: 'blur(8px)',
+                display: 'block',
+                duration: 0.25,
+              },
+              '<'
+            )
+            .set(screenRef.current, { display: 'none' });
+        });
+
+        previousModeRef.current = null;
+
+        return () => ctx.revert();
+      }
     }
   }, [menuMode, wrapperRef.current]);
 
-  // useEffect(() => {
-  //   if (!notifications?.length && showNotificationBody) {
-  //     setTriggerClose(true);
-  //     // setShowNotificationBody(false);
-  //   }
-  // }, [notifications, showNotificationBody]);
-
   return (
-    <div
-      id="notification-container"
-      ref={wrapperRef}
-      className="absolute translate-x-full opacity-95 right-0 w-fit z-50"
-    >
-      <div className="flex flex-col">
-        <div className="z-40 bg-primary-gray/20 self-end h-fit 3xl:w-[30vw] md:w-[40vw] 5xl:w-[20vw] xxs:w-[80vw] portrait:md:w-[60vw]  rounded-bl-sm px-3 pl-6 py-3">
-          <ul>
-            {notifications?.map((notification) => {
-              return (
-                <li key={notification.id}>
-                  {notification.notificationType === 'meetingInvite' && (
-                    <MeetingRequest
-                      notification={notification}
-                      setShowNotificationBody={setShowNotificationBody}
-                    />
-                  )}
-                  {notification.notificationType === 'ratingRequested' && (
-                    <RatingRequest notification={notification} />
-                  )}
-                  {notification.notificationType === 'inviteRejected' && (
-                    <MeetingRejected
-                      isDarkMode={isDarkMode}
-                      setIsDarkMode={setIsDarkMode}
-                      notification={notification}
-                    />
-                  )}
-                  {notification.notificationType === 'meetingCancelled' && (
-                    <MeetingCancelled
-                      isDarkMode={isDarkMode}
-                      setIsDarkMode={setIsDarkMode}
-                      notification={notification}
-                    />
-                  )}
-                  {/* {notification.notificationType === 'inviteAccepted' && (
+    <>
+      <div
+        ref={screenRef}
+        className="fixed bottom-0 h-screen w-screen backdrop-blur-0 bg-transparent z-20"
+        style={{ display: 'none' }}
+      ></div>
+      <div
+        id="notification-container"
+        ref={wrapperRef}
+        className="fixed translate-x-full opacity-95 right-0 w-fit z-50"
+      >
+        <div className="flex flex-col">
+          <div className="z-40 bg-primary-gray/20 self-end h-fit 3xl:w-[30vw] md:w-[40vw] 5xl:w-[20vw] xxs:w-[80vw] portrait:md:w-[60vw]  rounded-bl-sm px-3 pl-6 py-3">
+            <ul>
+              {notifications?.map((notification) => {
+                return (
+                  <li key={notification.id}>
+                    {notification.notificationType === 'meetingInvite' && (
+                      <MeetingRequest
+                        notification={notification}
+                        setShowNotificationBody={setShowNotificationBody}
+                        closeMenu={closeMenu}
+                      />
+                    )}
+                    {notification.notificationType === 'ratingRequested' && (
+                      <RatingRequest
+                        notification={notification}
+                        closeMenu={closeMenu}
+                      />
+                    )}
+                    {notification.notificationType === 'inviteRejected' && (
+                      <MeetingRejected
+                        isDarkMode={isDarkMode}
+                        setIsDarkMode={setIsDarkMode}
+                        notification={notification}
+                        closeMenu={closeMenu}
+                      />
+                    )}
+                    {notification.notificationType === 'meetingCancelled' && (
+                      <MeetingCancelled
+                        isDarkMode={isDarkMode}
+                        setIsDarkMode={setIsDarkMode}
+                        notification={notification}
+                        closeMenu={closeMenu}
+                      />
+                    )}
+                    {/* {notification.notificationType === 'inviteAccepted' && (
                       <MeetingAccepted
                         notification={notification}
                         setShowNotificationBody={setShowNotificationBody}
                       />
                     )} */}
-                  {notification.notificationType === 'newMessage' && (
-                    <NewMessageReceived
-                      isDarkMode={isDarkMode}
-                      setIsDarkMode={setIsDarkMode}
-                      notification={notification}
-                    />
-                  )}
-                  {notification.notificationType === 'currentMeeting' && (
-                    <CurrentMeeting
-                      isDarkMode={isDarkMode}
-                      setIsDarkMode={setIsDarkMode}
-                      notification={notification}
-                      meetings={meetings}
-                    />
-                  )}
-                </li>
-              );
-            })}
-          </ul>
+                    {notification.notificationType === 'newMessage' && (
+                      <NewMessageReceived
+                        isDarkMode={isDarkMode}
+                        setIsDarkMode={setIsDarkMode}
+                        notification={notification}
+                        closeMenu={closeMenu}
+                      />
+                    )}
+                    {notification.notificationType === 'currentMeeting' && (
+                      <CurrentMeeting
+                        isDarkMode={isDarkMode}
+                        setIsDarkMode={setIsDarkMode}
+                        notification={notification}
+                        meetings={meetings}
+                        closeMenu={closeMenu}
+                      />
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
