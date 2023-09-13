2dc5fad4d66452dccfde531265599ba8cf015ae4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-13 17:29:33 -0400

    adj bio height, +add resp styles

--- src/pages/aboutYourself/Bio.jsx
+++ src/pages/aboutYourself/Bio.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 const Bio = ({ setBio, bio, validBio }) => {
   return (
-    <div className="flex relative mb-9 lg:w-9/12 w-10/12 lg:basis-1/2 text-primary-gray self-center  h-[calc(100vh_-_65px)]">
+    <div className="flex relative mb-9 lg:w-9/12 w-10/12 lg:basis-1/2 text-primary-gray self-center  h-full">
       <label className="text-label block text-xs absolute -top-3 left-3 bg-white dark:bg-[#0a0908] px-1">
         Bio
       </label>
       <textarea
         type="text"
         rows="24"
-        className={`w-full px-4 rounded-2xl focus:outline-none border dark:text-white resize-none dark:border-white h-24 border-primary-gray text-sm py-2 scrollbar-hide bg-white dark:bg-[#0a0908] ${
+        className={`w-full px-4 rounded-2xl portrait:lg:h-56 landscape:6xl:h-80 focus:outline-none border dark:text-white resize-none dark:border-white h-24 border-primary-gray text-sm py-2 scrollbar-hide bg-white dark:bg-[#0a0908] ${
           validBio ? '' : 'border-headers'
         }`}
         onChange={(e) => {
