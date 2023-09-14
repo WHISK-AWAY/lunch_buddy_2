6d02f772d57bc107919be11cff8616c341d84180
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-14 17:00:47 -0400

    change notif mt basedon nav height

--- src/pages/NotificationCenter/ToastFeedback/ReportSubmitted.jsx
+++ src/pages/NotificationCenter/ToastFeedback/ReportSubmitted.jsx
@@ -15,7 +15,7 @@ export default function ReportSubmitted({ notification, t }) {
   return (
     <div
       id="meeting-card"
-      className="flex  text-xs text-primary-gray  rounded-sm pt-4 shadow-md items-center justify-between sticky px-3 py-2 3xl:w-[30vw] md:w-[40vw] 5xl:w-[20vw] w-[80vw] portrait:md:w-[60vw] bg-neutral-100/90 dark:bg-neutral-800/90 dark:text-white mt-10 portrait:md:mt-[9%] portrait:lg:mt-[7.9%] xs:mt-14 sm:mt-16 md:mt-11 lg:mt-[4.4%] xl:mt-[4.3%] 2xl:mt-[3.8%] 3xl:mt-[3.5%] 4xl:mt-[2.9%] 5xl:mt-[2.5%]  6xl:mt-[1.8%] -mr-4"
+      className="flex  text-xs text-primary-gray  rounded-sm pt-4 shadow-md items-center justify-between sticky px-3 py-2 3xl:w-[30vw] md:w-[40vw] 5xl:w-[20vw] w-[80vw] portrait:md:w-[60vw] bg-neutral-100/90 dark:bg-neutral-800/90 dark:text-white landscape:mt-10 portrait:mt-10 landscape:3xl:mt-12 -mr-4"
     >
       <div
         id="notification-details"
