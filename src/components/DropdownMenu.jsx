1bec37ed35b0984ea0b77b5878aa701979a761d1
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-14 15:43:05 -0400

    change drpdown height based on if user logged in ot not

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -180,7 +180,9 @@ const DropdownMenu = ({ menuMode, navHeight, closeMenu }) => {
       <div
         ref={wrapperRef}
         id="dropdown-container"
-        className={`dark:text-white dark:bg-[#0a0908]/60 bg-white/60 -translate-y-full fixed  w-screen opacity-95 z-30 landscape:lg:h-[60svh] landscape:h-[calc(100svh_-_56px)] portrait:h-[100svh] `}
+        className={` ${
+          !authUser.firstName ? 'landscape:lg:h-[40svh] ' : 'landscape:h-[60svh]'
+        } dark:text-white dark:bg-[#0a0908]/60 bg-white/60 -translate-y-full fixed  w-screen opacity-95 z-30 landscape:lg:h-[60svh] landscape:h-[calc(100svh_-_56px)] portrait:h-[100svh] `}
       >
         <ul className="flex flex-col items-center short:py-6  justify-center   overflow-y-auto portrait:h-full landscape:lg:h-full landscape:h-full align-center ">
           {!authUser.firstName ? (
