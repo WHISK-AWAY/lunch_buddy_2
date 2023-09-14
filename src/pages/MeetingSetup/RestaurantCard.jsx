58f8750aa8d005825096d70a2681f3c6c91466e4
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-13 23:54:35 -0400

    add resp landscape styles to rest card

--- src/pages/MeetingSetup/RestaurantCard.jsx
+++ src/pages/MeetingSetup/RestaurantCard.jsx
@@ -5,17 +5,17 @@ export default function RestaurantCard({ restaurant, chooseRestaurant }) {
   return (
     <div
       key={restaurant.id}
-      className="restaurant-card flex flex-row gap-8 bg-light-gray rounded-xl shadow-md p-4"
+      className="restaurant-card flex flex-row gap-8 bg-primary-gray/20 dark:bg-white/10 rounded-xl shadow-md p-4 dark:text-white text-primary-gray "
     >
       <div className="image-wrapper shrink-0 relative">
         <img
-          className="bg-white object-cover aspect-square w-32 h-32 rounded-[100%] z-10 p-1 relative self-end drop-shadow-md"
+          className="bg-white object-cover aspect-square w-28 h-28 rounded-[100%] z-10 p-1 relative self-end drop-shadow-md cursor-pointer"
           src={restaurant.image_url}
           alt="restaurant image provided by yelp"
         />
         <button
           onClick={(e) => chooseRestaurant(e, restaurant)}
-          className="button-round rounded-full w-12 aspect-square absolute -top-4 -left-4 flex justify-center items-center"
+          className="button-round rounded-full w-10 aspect-square absolute -top-3 -left-3 flex justify-center items-center"
         >
           <img
             src={plus}
@@ -24,15 +24,19 @@ export default function RestaurantCard({ restaurant, chooseRestaurant }) {
           />
         </button>
       </div>
-      <div className="info-wrapper">
+      <div className="info-wrapper text-center">
         <a href={restaurant.url} target="_blank" alt="">
-          <h2 className="text-headers font-semibold text-lg">
+          <h2 className="text-headers font-semibold text-base cursor-pointer">
             {restaurant.name}
           </h2>
         </a>
-        <p>{restaurant.location?.display_address?.join(' ')}</p>
-        <p>Rating: {restaurant.rating}</p>
-        <p>Reviews: {restaurant.review_count.toLocaleString()}</p>
+        <p className="text-[.8rem]">
+          {restaurant.location?.display_address?.join(' ')}
+        </p>
+        <p className="text-[.8rem]">Rating: {restaurant.rating}</p>
+        <p className="text-[.8rem]">
+          Reviews: {restaurant.review_count.toLocaleString()}
+        </p>
       </div>
     </div>
   );
