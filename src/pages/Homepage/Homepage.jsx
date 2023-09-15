ac3a62c56f59c803166b3b2ae6e5999fa3e5346f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-15 15:54:41 -0400

    finish LB hero section

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -18,177 +18,72 @@ const Homepage = () => {
   //   offset: 0,
   // });
 
-  useEffect(() => {
-    // fade bg image in only after it's downloaded
+  // useEffect(() => {
+  //   // fade bg image in only after it's downloaded
 
-    const bgImg = new Image();
-    bgImg.src = '/assets/bgImg/connect-q30.webp';
+  //   const bgImg = new Image();
+  //   bgImg.src = '/assets/bgImg/connect-q30.webp';
 
-    gsap.set(topImageRef.current, { opacity: 0 });
+  //   gsap.set(topImageRef.current, { opacity: 0 });
 
-    bgImg.onload = () => {
-      gsap.to(topImageRef.current, { opacity: 1, duration: 0.5 });
-    };
-  }, []);
+  //   bgImg.onload = () => {
+  //     gsap.to(topImageRef.current, { opacity: 1, duration: 0.5 });
+  //   };
+  // }, []);
 
   return (
-    <div className="flex md:w-screen w-full flex-col scroll-smooth gap-20 text-primary-gray pb-16 md:py-16 md:gap-56 h-fit overflow-hidden sm:gap-28 sm:py-16 py-20 bg-white dark:bg-[#0a0908] ">
-      <div
-        id="connect-section"
-        className="flex md:flex-row flex-col md:w-full md:h-screen md:pl-20 pr-6 py-6 sm:p-6"
-      >
+    <section className="homepage-wrapper w-[100svw] h-full dark:text-white text-[#0a0908] dark:bg-[#0a0908] bg-white ">
+      <div className="hero-container flex h-[100svh] w-full justify-center align-center custom-clip-path  ">
         <div
-          ref={topImageRef}
-          id="bg-img-container"
-          alt="two women holding coffee smiling at each other"
-          className="opacity-0 bg-[url('/assets/bgImg/connect.jpg')] supports-[background-image:_url('/assets/bgImg/connect-q30.webp')]:bg-[url('/assets/bgImg/connect-q30.webp')] self-right md:h-screen md:w-full md:bg-cover bg-cover h-[60svh] bg-no-repeat md:self-start sm:bg-right "
-          // data-aos="fade-up"
-          // data-aos-delay="400"
-          // duration="1000"
-          // data-aos-anchor-placement="center-center"
+          className="bg-cover bg-right  portrait:h-[calc(100svh_-_50px)]  bg-no-repeat
+      bg-[url('/assets/bgImg/homepage4.jpg')] portrait:bg-[url('/assets/bgImg/homepage4p.jpg')]  basis-[70%]  h-full portrait:lg:hidden portrait:basis-full portrait:bg-center "
         ></div>
-        <div
-          id="connect-text-section"
-          className="flex flex-col md:w-full justify-center"
-        >
-          <h1
-            id="connect-header"
-            className=" w-fit flex flex-col self-end  sm:self-center md:items-center md:justify-center text-justify self-right md:text-3xl text-xl py-4"
-            // data-aos="fade-down"
-            // data-aos-delay="700"
-          >
-            CONNECT
-          </h1>
-          <span
-            className="w-4/5  self-end text-justify text-xs leading-4 md:text-sm md:w-4/5 md:self-center md:text-center sm:w-11/12"
-            // data-aos="fade-in"
-            // data-aos-delay="1300"
-            // duration="500"
-          >
-            explore the seamless way to connect with like-minded people in your
-            area & build meaningful relationships
+
+        {/**
+    <video
+    src="/assets/bgImg/homepage6.mp4"
+    autoPlay={true}
+    loop={true}
+    muted={true}
+    className=" bg-cover h-[900px]"
+    />
+  */}
+
+        <div className="hero-text text-6xl basis-[30%] uppercase font-bold w-fit   portrait:basis-0  portrait:text-[4.8rem] ">
+          <span className="absolute top-[25%] portrait:top-20 right-[5%] portrait:right-[47%] portrait:sm:right-[38%]  portrait:w-[30vw] opacity-80 landscape:2xl:text-[6rem] landscape:2xl:top-[20%] landscape:5xl:text-[8rem] landscape:8xl:text-[10rem]">
+            lunch buddy
           </span>
+
+          <div className="lowercase font-light text-[.8rem] absolute  top-[37%] right-[14%] landscape:xl:text-[1rem] portrait:text-[1.2rem] portrait:tracking-widest portrait:top-[30%] landscape:2xl:text-[1.2rem]">
+            <p>expand your network,</p>
+            <p>one bite at the time</p>
+          </div>
+
+          <div className="font-regular landscape:xl:text-[1rem] text-[.8rem] top-[45%] right-[15%] border-2 rounded-sm border-primary-gray py-3 px-6  absolute portrait:text-[1rem] portrait:right-[23%] portrait:top-[39%] portrait:tracking-widest landscape:2xl:text-[1.2rem] landscape:2xl:right-[16%]">
+            <Link to="/signin">sign in </Link>
+          </div>
         </div>
       </div>
 
-      <div
-        id="explore-section"
-        className="flex md:flex-row-reverse md:w-full md:h-screen pl-6 sm:p-6 flex-col-reverse gap-4 md:pr-20"
-      >
-        <div
-          id="bg-img-container"
-          alt="dark plate on the table with toast on the bed of spinach, avocado slices and eggs on top"
-          className="md:w-1/2 bg-[url('/assets/bgImg/explore.jpg')] supports-[background-image:_url('/assets/bgImg/explore-q30.webp')]:bg-[url('/assets/bgImg/explore-q30.webp')] h-[70svh] md:h-screen bg-no-repeat bg-cover bg-left self-end w-full pl-6 md:self-end"
-          // data-aos="fade-right"
-          // data-aos-delay="300"
-          // data-aos-duration="1500"
-        ></div>
-        <div
-          id="explore-text-section"
-          className="flex flex-col justify-center md:w-1/2"
-        >
-          <h1
-            id="explore-header"
-            className="w-fit self-start sm:self-center md:text-3xl text-xl py-4"
-            // data-aos="fade-up"
-            // data-aos-delay="500"
-          >
-            EXPLORE
-          </h1>
-          <span
-            className="w-4/5 self-start md:self-center leading-4 md:text-sm sm:w-11/12 justify-center md:text-center sm:self-center text-xs text-justify pb-3 md:w-4/5"
-            // data-aos="zoom-in"
-            // data-aos-delay="400"
-            // data-aos-duration="2500"
-          >
-            discover new and exciting dining experiences, embark on a culinary
-            journey, explore diverse flavors and cuisines in your area
-          </span>
+      <div className="h-full relative bg-pink-200 overflow-hidden">
+        <div className="dark:bg-zinc-900/80 bg-zinc-200 h-[99vh] connect-section flex justify-between relative items-center ">
+          <div className="pt-28 pl-16 text-[4rem] font-bold uppercase opacity-80 landscape:2xl:text-[6rem] landscape:2xl:top-[20%] landscape:5xl:text-[8rem] landscape:8xl:text-[10rem]">
+            connect
+            <p className="lowercase font-light text-[.9rem] w-64">
+              meet new people based on shared hobbies, professionsal interests
+              and favourite cuisines
+            </p>
+          </div>
         </div>
+
+        <div className="h-full bg-contain w-[60%] bg-[url('/assets/bgImg/homepage1.jpg')] bg-no-repeat absolute top-16 -right-36"></div>
+
+        <div className="h-[400px] bg-orange-300 "></div>
+
+        <div>hello world</div>
+        <div className="dark:bg-zinc-900/60 bg-zinc-200 trio-section h-[400px]"></div>
       </div>
-
-      <div
-        id="expand-section"
-        className="flex md:flex-row flex-col md:w-full md:h-screen md:pl-20 pr-6 py-6 sm:p-6 gap-2"
-      >
-        <div
-          id="bg-img-container"
-          alt="two women giggling while sitting at the table in the cafe"
-          className="bg-[url('/assets/bgImg/expand.jpg')] supports-[background-image:_url('/assets/bgImg/expand-q30.webp')]:bg-[url('/assets/bgImg/expand-q30.webp')] h-[65svh] md:h-screen md:w-full bg-no-repeat bg-cover self-right sm:bg-right "
-          // data-aos="zoom-in-right"
-          // data-aos-delay="300"
-        ></div>
-
-        <div
-          id="expand-text-section"
-          className="flex flex-col w-full md:justify-center md:self-center self-end"
-        >
-          <h1
-            id="expand-header"
-            className="w-fit self-end flex flex-col sm:self-center justify-start md:text-3xl text-xl align-top md:self-center md:justify-center md:items-center py-1"
-            // data-aos="zoom-in"
-            // data-aos-delay="500"
-            // data-aos-duration="2000"
-          >
-            EXPAND
-          </h1>
-
-          <span
-            className="w-11/12 md:self-center md:text-center md:text-sm text-xs sm:text-center self-end text-right"
-            // data-aos="zoom-in-left"
-            // data-aos-delay="300"
-            // data-aos-duration="2000"
-          >
-            expand your network for growth and opportunities
-          </span>
-        </div>
-      </div>
-
-      <div
-        id="join-section"
-        className="flex md:flex-row-reverse md:w-full md:h-full flex-col pl-6 gap-4 sm:p-6 md:pr-20"
-      >
-        <div
-          id="bg-img-container"
-          alt="lady sitting at the table with her feet on the chair with a bowl of pasta, holding her phone in the left hand"
-          className="md:w-1/2 bg-[url('/assets/bgImg/join.jpg')] supports-[background-image:_url('/assets/bgImg/join-q30.webp')]:bg-[url('/assets/bgImg/join-q30.webp')] md:h-screen bg-no-repeat bg-right w-full bg-cover self-end h-[60svh]"
-          // data-aos="fade-left"
-          // data-aos-delay="300"
-          // data-aos-duration="1500"
-        ></div>
-        <div
-          id="join-text-section"
-          className="flex flex-col justify-center items-center pr-3 md:w-1/2"
-        >
-          <h1
-            id="join-header"
-            className="w-fit md:self-center  flex flex-col  md:text-3xl text-xl text-start pb-1"
-            // data-aos="fade-down"
-            // data-aos-delay="400"
-          >
-            JOIN
-          </h1>
-
-          <span
-            className="w-4/5 md:self-center flex flex-col items-center justify-center md:w-11/12 sm:self-center sm:w-11/12 text-xs pb-5"
-            // data-aos="fade-up"
-            // data-aos-delay="200"
-            // data-aos-duration="1000"
-          >
-            LUNCHbuddy and never eat lunch alone
-          </span>
-          <Link
-            to={auth.user?.id ? '/match' : '/register'}
-            className="border-2 self-center border-orange-400 rounded-3xl px-10 py-1 text-xs md:text-sm "
-            // data-aos="fade-in"
-            // data-aos-delay="1200"
-            // data-aos-duration="2900"
-          >
-            {auth.user?.id ? 'find buddy' : 'sign up'}
-          </Link>
-        </div>
-      </div>
-    </div>
+    </section>
   );
 };
 
