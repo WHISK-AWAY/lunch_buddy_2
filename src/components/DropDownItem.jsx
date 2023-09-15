168247684ba8f84c7da18baed14588d08d015bce
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-14 21:46:05 -0400

    add txt adjs

--- src/components/DropDownItem.jsx
+++ src/components/DropDownItem.jsx
@@ -5,7 +5,7 @@ const DropDownItem = (props) => {
   return (
     <li
       onClick={props.handleClick}
-      className="transition-all w-full text-center font-thin hover:bg-[#f4978e]/20 hover:ease-in-out duration-500 h-fit landscape:py-2"
+      className="transition-all w-full text-center font-thin hover:bg-[#f4978e]/20 hover:ease-in-out duration-500 h-fit landscape:py-2 portrait:py-2"
       style={{
         transitionDelay: '0.1s',
       }}
@@ -14,7 +14,7 @@ const DropDownItem = (props) => {
         to={props.linkTo}
         className="  block  align-middle  portrait:py-1 portrait:md:py-3 portrait:lg:py-4 landscape:xl:py-2"
       >
-        <div className=" text-2xl md:mt-0 short:mt-0 landscape:xl:text-[3.1rem] landscape:short:text-[2rem]  portrait:text-4xl short:text-xl portrait:md:text-5xl portrait:lg:text-6xl ">
+        <div className=" text-2xl md:mt-0 short:mt-0 landscape:xl:text-[3.1rem] landscape:short:text-[2rem]  portrait:text-[2.9rem] short:text-xl portrait:md:text-5xl portrait:lg:text-6xl ">
           {props.children.toUpperCase()}
         </div>
       </Link>
