ef51a6c722f77d59ca0ce1660e1bcc54cc2ae9c6
Patrick Bryan <pbryan9@gmail.com>
2023-09-16 11:44:39 -0700

    tidy class list (remove extra spaces)

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -4,7 +4,7 @@ const FormButton = (props) => {
   return (
     <button
       type="submit"
-      className="ease-in flex justify-center text-center 6xl:py-4 5xl:py-3 duration-300 w-full button 2xl:py-2  px-4 py-[.5rem] text-xs text-white  hover:bg-red-600 active:bg-red-700 transition-all hover:shadow-lg"
+      className="ease-in flex justify-center text-center 6xl:py-4 5xl:py-3 duration-300 w-full button 2xl:py-2 px-4 py-[.5rem] text-xs text-white hover:bg-red-600 active:bg-red-700 transition-all hover:shadow-lg"
       onClick={props.handleSubmit}
     >
       {props.children}
