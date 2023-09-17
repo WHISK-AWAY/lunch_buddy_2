42381cc6c574c9b81314db5e74251f5540f4093f
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-17 14:26:35 -0400

    change dropdown height for portrait

--- src/components/DropdownMenu.jsx
+++ src/components/DropdownMenu.jsx
@@ -132,7 +132,7 @@ const DropdownMenu = ({ menuMode, closeMenu }) => {
         id="dropdown-container"
         className={` ${
           !user.firstName ? 'landscape:lg:h-[40svh] ' : 'landscape:lg:h-[70svh]'
-        } dark:text-white dark:bg-[#0a0908]/60 bg-white/60 -translate-y-full fixed  w-screen opacity-95 z-30  landscape:h-[calc(100svh_-_56px)] portrait:h-[calc(100svh_-_56px)] `}
+        } dark:text-white dark:bg-[#0a0908]/60 bg-white/60 -translate-y-full fixed  w-screen opacity-95 z-30  landscape:h-[calc(100svh_-_56px)] portrait:h-[100svh] `}
       >
         <ul className="flex flex-col items-center short:py-6  justify-center  overflow-y-auto portrait:h-full landscape:lg:h-full landscape:h-full align-center ">
           {!user.firstName ? (
