ea62dc69c8f1cfa9056fe95f0cdb72f66adfd3ad
ruralFluffyDill <stacylukavsky@gmail.com>
2023-09-16 23:41:15 -0400

    rework dropdown

--- src/components/DropDownItem.jsx
+++ src/components/DropDownItem.jsx
@@ -14,7 +14,7 @@ const DropDownItem = (props) => {
         to={props.linkTo}
         className="block align-middle portrait:py-1 portrait:md:py-3 portrait:lg:py-4 landscape:xl:py-2"
       >
-        <div className="hover:scale-[1.02] flex items-center justify-center gap-4 hover:before:content-['•'] hover:after:content-['•'] before:text-2xl after:text-2xl before:font-roboto after:font-roboto hover:ease-in-out duration-300 text-2xl md:mt-0 short:mt-0 landscape:xl:text-[3.1rem] text-center landscape:short:text-[2rem]  portrait:text-[2.9rem]  short:text-xl portrait:md:text-5xl portrait:lg:text-6xl">
+        <div className="hover:scale-[1.02] flex items-center justify-center gap-4 hover:before:content-['•'] hover:after:content-['•'] before:text-2xl after:text-2xl before:font-roboto after:font-roboto hover:ease-in-out duration-300 text-2xl md:mt-0 short:mt-0 landscape:xl:text-[3.1rem] text-center landscape:short:text-[2rem]  portrait:text-[2.3rem]  short:text-xl portrait:md:text-5xl portrait:lg:text-6xl">
           {props.children.toUpperCase()}
         </div>
       </Link>
